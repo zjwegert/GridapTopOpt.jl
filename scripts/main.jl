62e786cd36982a250401f3427d3b15b22be2e6be
Z J Wegert <60646897+zjwegert@users.noreply.github.com>
2023-11-12 13:25:15 +1100

    added some stuff to main.jl

--- scripts/main.jl
+++ scripts/main.jl
@@ -1,17 +1,82 @@
+# function main()
 
+#     # Problem setup, defining the blocks
 
+#     φh
+#     caches = setup!(optimizer,...) # <- must include first iteration
+#     for k in 1:nsteps
+#         J_new,C_new,φh_new = step!(optimizer,...)
+#         # Logging using results
+#         # Check convergence criteria, using results
+#     end
 
+# end
 
-function main()
+function main_3D(mesh_partition,distribute)
+    ## Parameters
+    order = 1;
+    dom = (0,1,0,1,0,1);
+    el_size = (50,50,50);
+    # ...
+    ## FE Setup
+    model = CartesianDiscreteModel(ranks,mesh_partition,dom,el_size);
+    f_Γ_D(x) = (x[1] ≈ 0.0 && (x[2] <= ymax*prop_Γ_D + eps() || 
+        x[2] >= ymax-ymax*prop_Γ_D - eps()) && (x[3] <= zmax*prop_Γ_D + eps() ||
+        x[3] >= zmax-zmax*prop_Γ_D - eps())) ? true : false;
+    f_Γ_N(x) = (x[1] ≈ xmax && ymax/2-ymax*prop_Γ_N/4 - eps() <= x[2] <= 
+        ymax/2+ymax*prop_Γ_N/4 + eps() && zmax/2-zmax*prop_Γ_N/4 - eps() <= x[3]
+        <= zmax/2+zmax*prop_Γ_N/4 + eps()) ? true : false;
+    update_labels!(1,model,f_Γ_D,coord_max,"Gamma_D")
+    update_labels!(2,model,f_Γ_N,coord_max,"Gamma_N")
 
-    # Problem setup, defining the blocks
+    ## Triangulations and measures
+    Ω = Triangulation(model)
+    Γ_N = BoundaryTriangulation(model,tags="Gamma_N")
+    dΩ = Measure(Ω,2order)
+    dΓ_N = Measure(Γ_N,2order)
 
-    φh
-    caches = setup!(optimizer,...) # <- must include first iteration
+    ## Spaces
+    V = TestFESpace(model,reffe;dirichlet_tags=["Gamma_D"],dirichlet_masks=[true])
+    U = TrialFESpace(V,[0.0])
+    V_φ = TestFESpace(model,reffe)
+
+    ## Weak form
+    function a(u,v,ϕ,dΩ,dΓ_N)
+        _ϕh = ϕ_to_ϕₕ(ϕ,V_ϕ)
+        ∫((I ∘ _ϕh)*D*∇(u)⋅∇(v))dΩ
+    end
+    a(ϕ,dΩ,dΓ_N) = (u,v) -> a(u,v,ϕ,dΩ,dΓ_N)
+    l(v,ϕ,dΩ,dΓ_N) = ∫(v*g)dΓ_N
+    l(ϕ,dΩ,dΓ_N) = v -> l(v,ϕ,dΩ,dΓ_N)
+    res(u,v,ϕ,dΩ,dΓ_N) = a(u,v,ϕ,dΩ,dΓ_N) - l(v,ϕ,dΩ,dΓ_N)
+    # This large block is not very nice, we should clean this up.
+
+    ## Finite difference solver and level set function
+    # Note, γ (and therefore Δt) for advect and reinit will most likely be different.
+    # Will need to adjust Advection.jl to account for this - γ_advect, γ_reinit
+    stencil = FirstOrderStencil(D,γ);
+    adv_stencil = DistributedAdvectionStencil(stencil,model,max_steps,tol)
+    φh = interpolate(gen_lsf(4,0.2),V_φ)
+    φ = get_free_dof_values(φh)
+    reinit!(adv_stencil,φ,caches)
+
+    ## Optimisation functionals
+    J = (u,ϕ,dΩ,dΓ_N) -> a(u,u,ϕ,dΩ,dΓ_N)
+    v_J = u -> -D*∇(u)⋅∇(u) # <- without autodiff
+    C1 = (u,ϕ,dΩ,dΓ_N) -> ∫(((ρ ∘ ϕ) - 0.5)/vol_D)dΩ # <- volume constraint C1(...) = 0
+    v_C1 = u -> 1
+    optimiser = AugmentedLagrangian();
+
+    ## Hilbertian extension-regularisation problems
+    a_hilb = (p,q,dΩ)->∫(α^2*∇(p)⋅∇(q) + p*q)dΩ;
+    vel_ext = VelocityExtension(model,interp,a_hilb,1)
+    ## Solver
+    solver = PETScLinearSolver() # <- This will crash if outside GridapPETSc.with. Need to decide where this goes?
+    caches = setup!(optimizer,...) # <- Needs to run first iteration and get sensitivities etc.
+
     for k in 1:nsteps
         J_new,C_new,φh_new = step!(optimizer,...)
         # Logging using results
         # Check convergence criteria, using results
     end
-
 end
--- src/Optimiser.jl
+++ src/Optimiser.jl
@@ -50,7 +50,7 @@ function step!(m::AugmentedLagrangian,iter,J,C,caches)
     reinit!(φ,model,Δ,0.5,2000,reinit_tol)       
 
     ## Calculate objective and constraints
-    J_new,dJ,uh = obj(φh,g,mat,solve_data,interp)
-    C_new,dC = vol(φh,interp,solve_data.dΩ,solve_data.V_L2,vol_D)
+    J_new,v_J,uh = obj(φh,g,mat,solve_data,interp)
+    C_new,v_C = vol(φh,interp,solve_data.dΩ,solve_data.V_L2,vol_D)
     L_new = J_new - λ*(C_new - vf) + 1/(2Λ)*(C_new - vf)^2
 end
