fc3f2b14b4943ebc23ccd0a31dbdb7c03a6a6aaf
Z J Wegert <60646897+zjwegert@users.noreply.github.com>
2023-11-14 16:12:14 +1100

    project! now takes an FEFunction/s for RHS

--- src/VelocityExtension.jl
+++ src/VelocityExtension.jl
@@ -1,13 +1,3 @@
-# User supplies:
-#   model
-#   A(u,v,dΩ) (Usually ∫ β²∇u⋅∇v + uv dΩ)
-#   order -> fe order
-#
-# Optional:
-#   qorder -> quadrature order
-#   dirichlet_tags
-#   dirichlet_values 
-
 struct VelocityExtension{A,B,C,D,E,F}
     model   :: A
     spaces  :: B
@@ -40,28 +30,32 @@ function VelocityExtension(model,
     assem = SparseMatrixAssembler(Tm,Tv,U_reg,V_reg)
     K  = assemble_matrix((U,V) -> biform(U,V,dΩ),Hilb_assem,U_reg,V_reg)
     ns = numerical_setup(symbolic_setup(ls,K),K)
-    b  = pfill(PetscScalar(0.0),partition(axes(K,1)))
     x  = pfill(PetscScalar(0.0),partition(axes(K,2)))
 
-    spaces = (U_reg,V_reg); caches = (ns,b,x)
+    spaces = (U_reg,V_reg); caches = (ns,x)
     return VelocityExtension(model,spaces,interp,assem,dΩ,caches)
 end
 
-function project!(vel_ext::VelocityExtension,vh,vh_L2,φh)
-    U_reg, V_reg = vel_ext.spaces
-    ns, b, x = vel_ext.caches
-    dΩ = vel_ext.measure; assem = vel_ext.assem; DH = vel_ext.interp.DH
-    dJ(v) = ∫(-vh_L2⋅v⋅(DH ∘ φh)⋅(norm ∘ ∇(φh)))*dΩ;
-    assemble_vector!(b,dJ,assem,V_reg)
-    solve!(x,ns,b)
-    copy!(get_free_dof_values(vh),x)
-    consistent!(get_free_dof_values(vh)) |> fetch # From matrix layout to finite element layout
-    return nothing
+function project!(vel_ext::VelocityExtension,dJh::GridapDistributed.DistributedCellField)
+    ns, x = vel_ext.caches
+    solve!(x,ns,-get_free_dof_values(dJh))
+    copy!(get_free_dof_values(dJh),x)
+    consistent!(get_free_dof_values(dJh)) |> fetch # From matrix layout to finite element layout
+    return dJh
 end
 
-function project(vel_ext::VelocityExtension,vh_L2,φh)
-    U_reg, V_reg = vel_ext.spaces
-    vh = zero(U_reg)
-    project!(vel_ext,vh,vh_L2,φh)
-    return vh
+function project!(vel_ext::VelocityExtension,dJh::FEFunction)
+    ns, x = vel_ext.caches
+    solve!(x,ns,-get_free_dof_values(dJh))
+    copy!(get_free_dof_values(dJh),x)
+    return dJh
 end
+
+project!(vel_ext::VelocityExtension,dJh_vec::Vector) = map(dJh -> project!(vel_ext,dJh),dJh_vec)
+
+# function project(vel_ext::VelocityExtension,dJh)
+#     U_reg, _ = vel_ext.spaces
+#     vh = zero(U_reg)
+#     project!(vel_ext,dJh)
+#     return vh
+# end
