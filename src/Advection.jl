f1da5aa70bf305d3aff1d15a198c92fd49323cd2
JordiManyer <jordi.manyer@monash.edu>
2023-11-14 14:49:57 +1100

    Added permutation for high order advection

--- src/Advection.jl
+++ src/Advection.jl
@@ -136,82 +136,154 @@ end
 
 # Distributed advection stencil
 
-struct DistributedAdvectionStencil
-  stencil :: AdvectionStencil
-  model
-  max_steps
-  tol
+struct DistributedAdvectionStencil{O}
+  stencil   :: AdvectionStencil
+  model     :: DistributedDiscreteModel
+  space     :: DistributedFESpace
+  perm      :: Vector
+  max_steps :: Int
+  tol       :: Real
   Δ
   local_sizes
 end
 
 function AdvectionStencil(stencil::AdvectionStencil,
                           model::DistributedDiscreteModel,
-                          max_steps::Int,
-                          tol::T) where T
-  local_sizes, local_Δ = map(local_views(model)) do model
+                          space::DistributedFESpace,
+                          max_steps::Int,tol::T) where T
+  order = get_order(first(Gridap.CellData.get_data(get_fe_basis(V))))
+  local_sizes, local_Δ, perm = map(local_views(model),local_views(space)) do model, space
     desc = get_cartesian_descriptor(model)
-    return desc.partition .+ 1, desc.sizes
-  end
+    dof_permutation = create_dof_permutation(model,space,order)
+    return desc.partition .+ 1, desc.sizes, dof_permutation
+  end |> PartitionedArrays.tuple_of_arrays
   Δ = PartitionedArrays.getany(local_Δ)
-  return DistributedAdvectionStencil(stencil,model,max_steps,tol,Δ,local_sizes)
+  return DistributedAdvectionStencil{order}(stencil,model,space,perm,max_steps,tol,Δ,local_sizes)
 end
 
-function allocate_caches(s::DistributedAdvectionStencil,φ::PVector,vel::PVector)
+Gridap.ReferenceFEs.get_order(f::Gridap.Fields.LinearCombinationFieldVector) = get_order(f.fields)
+
+function create_dof_permutation(model::CartesianDiscreteModel{Dc},
+                                space::UnconstrainedFESpace,
+                                order::Integer) where Dc
+  function get_terms(poly::Polytope, orders)
+    _nodes, facenodes = Gridap.ReferenceFEs._compute_nodes(poly, orders)
+    terms = Gridap.ReferenceFEs._coords_to_terms(_nodes, orders)
+    return terms
+  end
+  desc = get_cartesian_descriptor(model)
+  
+  ncells = desc.partition
+  ndofs  = order .* ncells .+ 1
+  @check prod(ndofs) == num_free_dofs(space)
+
+  new_dof_ids  = LinearIndices(ndofs)
+  #o2n_dof_map = fill(-1,num_free_dofs(V))
+  n2o_dof_map = fill(-1,num_free_dofs(V))
+
+  terms = get_terms(poly, fill(order,Dc))
+  cell_dof_ids = get_cell_dof_ids(V)
+  cache_cell_dof_ids = array_cache(cell_dof_ids)
+  for (iC,cell) in enumerate(CartesianIndices(nc))
+    first_new_dof  = order .* (Tuple(cell) .- 1) .+ 1
+    new_dofs_range = map(i -> i:i+order,first_new_dof)
+    new_dofs = view(new_dof_ids,new_dofs_range...)
+
+    cell_dofs = getindex!(cache_cell_dof_ids,cell_dof_ids,iC)
+    for (iDof, dof) in enumerate(cell_dofs)
+      t = terms[iDof]
+      #o2n_dof_map[dof] = new_dofs[t]
+      n2o_dof_map[new_dofs[t]] = dof
+    end
+  end
+
+  return n2o_dof_map
+end
+
+function allocate_caches(s::DistributedAdvectionStencil{O},φ::PVector,vel::PVector) where O
   local_caches = map(local_views(φ),local_views(vel)) do φ,vel
     allocate_caches(s.stencil,φ,vel)
   end
   φ_tmp   = similar(φ)
   vel_tmp = similar(vel)
-  return φ_tmp, vel_tmp, local_caches
+  perm_caches = (O >= 2) ? (similar(φ), similar(vel)) : nothing
+  return φ_tmp, vel_tmp, perm_caches, local_caches
 end
 
-function advect!(s::DistributedAdvectionStencil,φ::PVector,vel::PVector,caches)
-  _, _, local_caches = caches
+function permute!(x_out,x_in,perm)
+  for (i_new,i_old) in enumerate(perm)
+    x_out[i_new] = x_in[i_old]
+  end
+  return x_out
+end
+function permute!(x_out::PVector,x_in::PVector,perm) 
+  map(permute!,partition(x_out),partition(x_in),perm)
+  return x_out
+end
+
+function permute_inv!(x_out,x_in,perm)
+  for (i_new,i_old) in enumerate(perm)
+    x_out[i_old] = x_in[i_new]
+  end
+  return x_out
+end
+function permute_inv!(x_out::PVector,x_in::PVector,perm) 
+  map(permute_inv!,partition(x_out),partition(x_in),perm)
+  return x_out
+end
+
+function advect!(s::DistributedAdvectionStencil{O},φ::PVector,vel::PVector,caches) where O
+  _, _, perm_caches, local_caches = caches
+  
+  _φ   = (O >= 2) ? permute!(perm_caches[1],φ,s.perm) : φ
+  _vel = (O >= 2) ? permute!(perm_caches[2],vel,s.perm) : vel
 
   ## CFL Condition (requires γ≤1.0)
   Δt = compute_Δt(s.stencil,φ,vel)
   for _ ∈ Base.OneTo(s.max_steps)
     # Apply operations across partitions
-    map(local_views(φ),local_views(),local_views(vel),local_caches,s.local_sizes) do φ,vel,caches,S
-      _φ   = reshape(φ,S)
-      _vel = reshape(vel,S)
-      advect!(s.stencil,_φ,_vel,s.Δ,Δt,caches)
+    map(local_views(_φ),local_views(_vel),local_caches,s.local_sizes) do _φ,_vel,caches,S
+      φ_mat   = reshape(_φ,S)
+      vel_mat = reshape(_vel,S)
+      advect!(s.stencil,φ_mat,vel_mat,s.Δ,Δt,caches)
     end
     # Update ghost nodes
-    consistent!(φ) |> fetch
+    consistent!(_φ) |> fetch
   end
+  φ = (O >= 2) ? permute_inv!(φ,_φ,s.perm) : _φ
   return φ
 end
 
-function reinit!(s::DistributedAdvectionStencil,φ::PVector,vel::PVector,caches)
-  φ_tmp, vel_tmp, local_caches = caches
+function reinit!(s::DistributedAdvectionStencil,φ::PVector,caches)
+  φ_tmp, vel_tmp, perm_caches, local_caches = caches
+  _φ = (O >= 2) ? permute!(perm_caches[1],φ,s.perm) : φ
 
   # Compute approx sign function S
-  vel_tmp .= @. φ / sqrt(φ*φ + prod(Δ))
+  vel_tmp .= @. _φ / sqrt(_φ*_φ + prod(Δ))
 
   ## CFL Condition (requires γ≤0.5)
-  Δt = compute_Δt(s.stencil,φ,1.0) # As inform(vel_tmp) = 1.0
+  Δt = compute_Δt(s.stencil,_φ,1.0) # As inform(vel_tmp) = 1.0
 
   # Apply operations across partitions
   step = 1; err = maximum(abs,φ); fill!(φ_tmp,0.0)
   while (err > tol) && (step <= max_steps) 
     # Step of 1st order upwind reinitialisation equation
-    map(local_views(φ_tmp),local_views(φ),local_views(vel_tmp),local_caches,s.local_sizes) do φ_tmp,φ,vel_tmp,local_caches,S
-      _φ_tmp   = reshape(φ,S)
-      _φ       = reshape(φ,S)
-      _vel_tmp = reshape(vel,S)
-      reinit!(s.stencil,_φ_tmp,_φ,_vel_tmp,s.Δ,Δt,local_caches)
+    map(local_views(φ_tmp),local_views(_φ),local_views(vel_tmp),local_caches,s.local_sizes) do φ_tmp,φ,vel_tmp,local_caches,S
+      φ_tmp_mat   = reshape(φ,S)
+      φ_mat       = reshape(φ,S)
+      vel_tmp_mat = reshape(vel_tmp,S)
+      reinit!(s.stencil,φ_tmp_mat,φ_mat,vel_tmp_mat,s.Δ,Δt,local_caches)
     end
 
     # Compute error
-    φ .-= φ_tmp # φ - φ_tmp
-    err = maximum(abs,φ) # Ghosts not needed yet: partial maximums computed using owned values only. 
+    _φ .-= φ_tmp # φ - φ_tmp
+    err = maximum(abs,_φ) # Ghosts not needed yet: partial maximums computed using owned values only. 
     step += 1
 
     # Update φ
-    copy!(φ,φ_tmp)
-    consistent!(φ) |> fetch # We exchange ghosts here!
+    copy!(_φ,φ_tmp)
+    consistent!(_φ) |> fetch # We exchange ghosts here!
   end
-  return φ # Should this be φ_new for consistency? 
+  φ = (O >= 2) ? permute_inv!(φ,_φ,s.perm) : _φ
+  return φ
 end
