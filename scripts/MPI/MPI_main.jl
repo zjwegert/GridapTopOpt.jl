556af6cb241fa8a787a0da633510ab4601b698c2
JordiManyer <jordi.manyer@monash.edu>
2024-01-10 11:51:51 +1100

    Started updating MPI scripts

--- scripts/MPI/MPI_main.jl
+++ scripts/MPI/MPI_main.jl
@@ -13,28 +13,26 @@ function main(mesh_partition,distribute)
   ranks = distribute(LinearIndices((prod(mesh_partition),)))
 
   ## Parameters
-  order = 1;
+  order = 1
   xmax=ymax=1.0
-  prop_Γ_N = 0.4;
+  prop_Γ_N = 0.4
   prop_Γ_D = 0.2
-  dom = (0,xmax,0,ymax);
-  el_size = (400,400);
-  γ = 0.1;
-  γ_reinit = 0.5;
+  dom = (0,xmax,0,ymax)
+  el_size = (400,400)
+  γ = 0.1
+  γ_reinit = 0.5
   max_steps = floor(Int,minimum(el_size)/10)
   tol = 1/(order^2*10)*prod(inv,minimum(el_size)) # <- change to 1/order^2*prod(...) ?
-  D = 1;
-  η_coeff = 2;
-  α_coeff = 4;
+  D = 1
+  η_coeff = 2
+  α_coeff = 4
   path = dirname(dirname(@__DIR__))*"/results/MPI_main"
 
   ## FE Setup
   model = CartesianDiscreteModel(ranks,mesh_partition,dom,el_size);
   Δ = get_Δ(model)
-  f_Γ_D(x) = (x[1] ≈ 0.0 && (x[2] <= ymax*prop_Γ_D + eps() ||
-    x[2] >= ymax-ymax*prop_Γ_D - eps())) ? true : false;
-  f_Γ_N(x) = (x[1] ≈ xmax && ymax/2-ymax*prop_Γ_N/4 - eps() <= x[2] <=
-    ymax/2+ymax*prop_Γ_N/4 + eps()) ? true : false;
+  f_Γ_D(x) = (x[1] ≈ 0.0) && ((x[2] <= ymax*prop_Γ_D + eps()) || (x[2] >= ymax-ymax*prop_Γ_D - eps()))
+  f_Γ_N(x) = (x[1] ≈ xmax) && (ymax/2-ymax*prop_Γ_N/4 - eps() <= x[2] <= ymax/2+ymax*prop_Γ_N/4 + eps())
   update_labels!(1,model,f_Γ_D,"Gamma_D")
   update_labels!(2,model,f_Γ_N,"Gamma_N")
 
@@ -53,8 +51,7 @@ function main(mesh_partition,distribute)
   U_reg = TrialFESpace(V_reg,0)
 
   ## Create FE functions
-  φh = interpolate(gen_lsf(4,0.2),V_φ);
-  φ = get_free_dof_values(φh)
+  φh = interpolate(gen_lsf(4,0.2),V_φ)
 
   ## Interpolation and weak form
   interp = SmoothErsatzMaterialInterpolation(η = η_coeff*maximum(Δ))
@@ -62,19 +59,18 @@ function main(mesh_partition,distribute)
 
   a(u,v,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*D*∇(u)⋅∇(v))dΩ
   l(v,φ,dΩ,dΓ_N) = ∫(v)dΓ_N
-  res(u,v,φ,dΩ,dΓ_N) = a(u,v,φ,dΩ,dΓ_N) - l(v,φ,dΩ,dΓ_N)
 
   ## Optimisation functionals
-  ξ = 0.3;
-  J = (u,φ,dΩ,dΓ_N) -> ∫((I ∘ φ)*D*∇(u)⋅∇(u) + ξ*(ρ ∘ φ))dΩ
-  dJ = (q,u,φ,dΩ,dΓ_N) -> ∫((ξ-D*∇(u)⋅∇(u))*q*(DH ∘ φ)*(norm ∘ ∇(φ)))dΩ;
+  ξ = 0.3
+  J(u,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*D*∇(u)⋅∇(u) + ξ*(ρ ∘ φ))dΩ
+  dJ(q,u,φ,dΩ,dΓ_N) = ∫((ξ-D*∇(u)⋅∇(u))*q*(DH ∘ φ)*(norm ∘ ∇(φ)))dΩ;
 
   ## Finite difference solver and level set function
-  stencil = AdvectionStencil(FirstOrderStencil(2,Float64),model,V_φ,Δ./order,max_steps,tol)
-  reinit!(stencil,φ,γ_reinit)
+  stencil = AdvectionStencil(FirstOrderStencil(2,Float64),model,V_φ,tol,max_steps)
+  reinit!(stencil,φh,γ_reinit)
 
   ## Setup solver and FE operators
-  state_map = AffineFEStateMap(a,l,res,U,V,V_φ,U_reg,φh,dΩ,dΓ_N)
+  state_map = AffineFEStateMap(a,l,U,V,V_φ,U_reg,φh,dΩ,dΓ_N)
   pcfs = PDEConstrainedFunctionals(J,state_map,analytic_dJ=dJ)
 
   ## Hilbertian extension-regularisation problems
@@ -84,21 +80,13 @@ function main(mesh_partition,distribute)
 
   ## Optimiser
   make_dir(path;ranks=ranks)
-  _conv_cond = t->LSTO_Distributed.conv_cond(t;coef=1/50);
-  optimiser = AugmentedLagrangian(φ,pcfs,stencil,vel_ext,interp,el_size,γ,γ_reinit,conv_criterion=_conv_cond);
-  for history in optimiser
-    it,Ji,_,_ = last(history)
-    print_history(it,["J"=>Ji];ranks=ranks)
-    write_history(history,path*"/history.csv";ranks=ranks)
-    write_vtk(Ω,path*"/struc_$it",it,["phi"=>φh,"H(phi)"=>(H ∘ φh),"|nabla(phi))|"=>(norm ∘ ∇(φh))])
+  converged(m) = LSTO_Distributed.default_al_converged(m;L_tol=0.02*maximum(Δ))
+  optimiser = AugmentedLagrangian(pcfs,stencil,vel_ext,φh;γ,γ_reinit,converged,verbose=true)
+  for (it, uh, φh) in optimiser
+    write_vtk(Ω,path*"/struc_$it",it,["phi"=>φh,"H(phi)"=>(H ∘ φh),"|nabla(phi))|"=>(norm ∘ ∇(φh)),"uh"=>uh])
   end
-  it,Ji,_,_ = last(optimiser.history)
-  print_history(it,["J"=>Ji];ranks=ranks)
-  write_history(optimiser.history,path*"/history.csv";ranks=ranks)
-  uhi = get_state(pcfs)
-  write_vtk(Ω,path*"/struc_$it",it,["phi"=>φh,"H(phi)"=>(H ∘ φh),"|nabla(phi))|"=>(norm ∘ ∇(φh)),"uh"=>uhi];iter_mod=1)
 end
 
 with_mpi() do distribute
   main((2,3),distribute)
-end;
+end
