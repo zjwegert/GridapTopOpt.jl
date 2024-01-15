1b1fa07e9c6600387ccc9a34f0a1a1808a8acba9
Z J Wegert (Workstation) <zach.wegert@hdr.qut.edu.au>
2024-01-15 15:19:18 +1000

    Added benchmark scripts

---
+++ scripts/Benchmarks/benchmark.jl
@@ -0,0 +1,333 @@
+using Gridap, Gridap.MultiField, GridapDistributed, GridapPETSc, GridapSolvers, 
+  PartitionedArrays, LSTO_Distributed, SparseMatricesCSR
+
+using GridapSolvers: NewtonSolver
+
+global NAME = ARGS[1]
+global PROB_TYPE = ARGS[2]
+global N = parse(Int,ARGS[3])
+global N_EL = parse(Int,ARGS[4])
+global ORDER = parse(Int,ARGS[5])
+global VERBOSE = parse(Int,ARGS[6])
+
+function nl_elast(mesh_partition,ranks,el_size,order,verbose)
+  ## Parameters
+  xmax=ymax=zmax=1.0
+  prop_Γ_N = 0.4
+  dom = (0,xmax,0,ymax,0,zmax)
+  γ = 0.1
+  γ_reinit = 0.5
+  max_steps = floor(Int,minimum(el_size)/3)
+  tol = 1/(10order^2)*prod(inv,minimum(el_size))
+  η_coeff = 2
+  α_coeff = 4
+
+  ## FE Setup
+  model = CartesianDiscreteModel(ranks,mesh_partition,dom,el_size)
+  Δ = get_Δ(model)
+  f_Γ_D(x) = (x[1] ≈ 0.0)
+  f_Γ_N(x) = (x[1] ≈ xmax) && (ymax/2-ymax*prop_Γ_N/4 - eps() <= x[2] <= ymax/2+ymax*prop_Γ_N/4 + eps()) &&
+      (zmax/2-zmax*prop_Γ_N/4 - eps() <= x[3] <= zmax/2+zmax*prop_Γ_N/4 + eps())
+  update_labels!(1,model,f_Γ_D,"Gamma_D")
+  update_labels!(2,model,f_Γ_N,"Gamma_N")
+
+  ## Triangulations and measures
+  Ω = Triangulation(model)
+  Γ_N = BoundaryTriangulation(model,tags="Gamma_N")
+  dΩ = Measure(Ω,2order)
+  dΓ_N = Measure(Γ_N,2order)
+
+  ## Spaces
+  reffe = ReferenceFE(lagrangian,VectorValue{3,Float64},order)
+  reffe_scalar = ReferenceFE(lagrangian,Float64,order)
+  V = TestFESpace(model,reffe;dirichlet_tags=["Gamma_D"])
+  U = TrialFESpace(V,VectorValue(0.0,0.0,0.0))
+  V_φ = TestFESpace(model,reffe_scalar)
+  V_reg = TestFESpace(model,reffe_scalar;dirichlet_tags=["Gamma_N"])
+  U_reg = TrialFESpace(V_reg,0)
+
+  ## Create FE functions
+  φh = interpolate(gen_lsf(4,0.2),V_φ)
+
+  ## Interpolation and weak form
+  interp = SmoothErsatzMaterialInterpolation(η = η_coeff*maximum(Δ))
+  I,H,DH,ρ = interp.I,interp.H,interp.DH,interp.ρ
+
+  _E = 1000
+  nu = 0.3
+  μ, λ = _E/(2*(1 + nu)), _E*nu/((1 + nu)*(1 - 2*nu))
+  g = VectorValue(0,0,-100)
+  # Deformation gradient
+  F(∇u) = one(∇u) + ∇u'
+  J(F) = sqrt(det(C(F)))
+  # Derivative of green Strain
+  dE(∇du,∇u) = 0.5*( ∇du⋅F(∇u) + (∇du⋅F(∇u))' )
+  # Right Caughy-green deformation tensor
+  C(F) = (F')⋅F
+  # Constitutive law (Neo hookean)
+  function S(∇u)
+    Cinv = inv(C(F(∇u)))
+    μ*(one(∇u)-Cinv) + λ*log(J(F(∇u)))*Cinv
+  end
+  # Cauchy stress tensor
+  σ(∇u) = (1.0/J(F(∇u)))*F(∇u)⋅S(∇u)⋅(F(∇u))'
+  res(u,v,φ,dΩ,dΓ_N) = ∫( (I ∘ φ)*((dE∘(∇(v),∇(u))) ⊙ (S∘∇(u))) )*dΩ - ∫(g⋅v)dΓ_N
+
+  ## Optimisation functionals
+  ξ = 0.5
+  Obj(u,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*((dE∘(∇(u),∇(u))) ⊙ (S∘∇(u))) + ξ*(ρ ∘ φ))dΩ
+
+  ## Finite difference solver and level set function
+  stencil = AdvectionStencil(FirstOrderStencil(3,Float64),model,V_φ,tol,max_steps)
+  reinit!(stencil,φh,γ_reinit)
+
+  ## Setup solver and FE operators
+  Tm = SparseMatrixCSR{0,PetscScalar,PetscInt}
+  Tv = Vector{PetscScalar}
+  lin_solver = ElasticitySolver(V)
+  nl_solver = NewtonSolver(lin_solver;maxiter=50,rtol=10^-8,verbose=verbose)
+
+  state_map = NonlinearFEStateMap(
+    res,U,V,V_φ,U_reg,φh,dΩ,dΓ_N;
+    assem_U = SparseMatrixAssembler(Tm,Tv,U,V),
+    assem_adjoint = SparseMatrixAssembler(Tm,Tv,V,U),
+    assem_deriv = SparseMatrixAssembler(Tm,Tv,U_reg,U_reg),
+    nls = nl_solver, adjoint_ls = lin_solver
+  )
+  pcfs = PDEConstrainedFunctionals(Obj,state_map)
+
+  ## Hilbertian extension-regularisation problems
+  α = α_coeff*maximum(Δ)
+  a_hilb(p,q) = ∫(α^2*∇(p)⋅∇(q) + p*q)dΩ;
+  vel_ext = VelocityExtension(
+    a_hilb,U_reg,V_reg;
+    assem = SparseMatrixAssembler(Tm,Tv,U_reg,V_reg),
+    ls = PETScLinearSolver()
+  )
+  
+  ## Optimiser
+  return AugmentedLagrangian(pcfs,stencil,vel_ext,φh;γ,γ_reinit,verbose=verbose)
+end
+
+function therm(mesh_partition,ranks,el_size,order,verbose)
+  ## Parameters
+  xmax=ymax=zmax=1.0
+  prop_Γ_N = 0.4
+  prop_Γ_D = 0.2
+  dom = (0,xmax,0,ymax,0,zmax)
+  γ = 0.1
+  γ_reinit = 0.5
+  max_steps = floor(Int,minimum(el_size)/3)
+  tol = 1/(order^2*10)*prod(inv,minimum(el_size))
+  D = 1
+  η_coeff = 2
+  α_coeff = 4
+
+  ## FE Setup
+  model = CartesianDiscreteModel(ranks,mesh_partition,dom,el_size);
+  Δ = get_Δ(model)
+  f_Γ_D(x) = (x[1] ≈ 0.0) && (x[2] <= ymax*prop_Γ_D + eps() || x[2] >= ymax-ymax*prop_Γ_D - eps()) &&
+    (x[3] <= zmax*prop_Γ_D + eps() || x[3] >= zmax-zmax*prop_Γ_D - eps())
+  f_Γ_N(x) = (x[1] ≈ xmax) && (ymax/2-ymax*prop_Γ_N/4 - eps() <= x[2] <= ymax/2+ymax*prop_Γ_N/4 + eps()) &&
+    (zmax/2-zmax*prop_Γ_N/4 - eps() <= x[3] <= zmax/2+zmax*prop_Γ_N/4 + eps())
+  update_labels!(1,model,f_Γ_D,"Gamma_D")
+  update_labels!(2,model,f_Γ_N,"Gamma_N")
+
+  ## Triangulations and measures
+  Ω = Triangulation(model)
+  Γ_N = BoundaryTriangulation(model,tags="Gamma_N")
+  dΩ = Measure(Ω,2order)
+  dΓ_N = Measure(Γ_N,2order)
+
+  ## Spaces
+  reffe_scalar = ReferenceFE(lagrangian,Float64,order)
+  V = TestFESpace(model,reffe_scalar;dirichlet_tags=["Gamma_D"])
+  U = TrialFESpace(V,0.0)
+  V_φ = TestFESpace(model,reffe_scalar)
+  V_reg = TestFESpace(model,reffe_scalar;dirichlet_tags=["Gamma_N"])
+  U_reg = TrialFESpace(V_reg,0)
+
+  ## Create FE functions
+  φh = interpolate(gen_lsf(4,0.2),V_φ);
+
+  ## Interpolation and weak form
+  interp = SmoothErsatzMaterialInterpolation(η = η_coeff*maximum(Δ))
+  I,H,DH,ρ = interp.I,interp.H,interp.DH,interp.ρ
+
+  a(u,v,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*D*∇(u)⋅∇(v))dΩ
+  l(v,φ,dΩ,dΓ_N) = ∫(v)dΓ_N
+
+  ## Optimisation functionals
+  ξ = 0.1
+  J(u,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*D*∇(u)⋅∇(u) + ξ*(ρ ∘ φ))dΩ
+  dJ(q,u,φ,dΩ,dΓ_N) = ∫((ξ-D*∇(u)⋅∇(u))*q*(DH ∘ φ)*(norm ∘ ∇(φ)))dΩ
+
+  ## Finite difference solver and level set function
+  stencil = AdvectionStencil(FirstOrderStencil(3,Float64),model,V_φ,tol,max_steps)
+  reinit!(stencil,φh,γ_reinit)
+
+  ## Setup solver and FE operators
+  Tm = SparseMatrixCSR{0,PetscScalar,PetscInt}
+  Tv = Vector{PetscScalar}
+  solver = PETScLinearSolver()
+  
+  state_map = AffineFEStateMap(
+    a,l,U,V,V_φ,U_reg,φh,dΩ,dΓ_N;
+    assem_U = SparseMatrixAssembler(Tm,Tv,U,V),
+    assem_adjoint = SparseMatrixAssembler(Tm,Tv,V,U),
+    assem_deriv = SparseMatrixAssembler(Tm,Tv,U_reg,U_reg),
+    ls = solver,adjoint_ls = solver
+  )
+  pcfs = PDEConstrainedFunctionals(J,state_map,analytic_dJ=dJ)
+
+  ## Hilbertian extension-regularisation problems
+  α = α_coeff*maximum(Δ)
+  a_hilb(p,q) = ∫(α^2*∇(p)⋅∇(q) + p*q)dΩ;
+  vel_ext = VelocityExtension(
+    a_hilb, U_reg, V_reg;
+    assem = SparseMatrixAssembler(Tm,Tv,U_reg,V_reg),
+    ls = solver
+  )
+
+  ## Optimiser
+  return AugmentedLagrangian(pcfs,stencil,vel_ext,φh;γ,γ_reinit,verbose=verbose)
+end
+
+function main(mesh_partition,ranks,el_size,order,verbose)
+  ## Parameters
+  xmax=ymax=zmax=1.0
+  prop_Γ_N = 0.4
+  dom = (0,xmax,0,ymax,0,zmax)
+  γ = 0.1
+  γ_reinit = 0.5
+  max_steps = floor(Int,minimum(el_size)/3)
+  tol = 1/(order^2*10)*prod(inv,minimum(el_size))
+  C = isotropic_3d(1.,0.3)
+  g = VectorValue(0,0,-1)
+  η_coeff = 2
+  α_coeff = 4
+
+  ## FE Setup
+  model = CartesianDiscreteModel(ranks,mesh_partition,dom,el_size);
+  Δ = get_Δ(model)
+  f_Γ_D(x) = (x[1] ≈ 0.0)
+  f_Γ_N(x) = (x[1] ≈ xmax) && (ymax/2-ymax*prop_Γ_N/4 - eps() <= x[2] <= ymax/2+ymax*prop_Γ_N/4 + eps()) &&
+      (zmax/2-zmax*prop_Γ_N/4 - eps() <= x[3] <= zmax/2+zmax*prop_Γ_N/4 + eps())
+  update_labels!(1,model,f_Γ_D,"Gamma_D")
+  update_labels!(2,model,f_Γ_N,"Gamma_N")
+
+  ## Triangulations and measures
+  Ω = Triangulation(model)
+  Γ_N = BoundaryTriangulation(model,tags="Gamma_N")
+  dΩ = Measure(Ω,2*order)
+  dΓ_N = Measure(Γ_N,2*order)
+  vol_D = sum(∫(1)dΩ)
+
+  ## Spaces
+  reffe = ReferenceFE(lagrangian,VectorValue{3,Float64},order)
+  reffe_scalar = ReferenceFE(lagrangian,Float64,order)
+  V = TestFESpace(model,reffe;dirichlet_tags=["Gamma_D"])
+  U = TrialFESpace(V,VectorValue(0.0,0.0,0.0))
+  V_φ = TestFESpace(model,reffe_scalar)
+  V_reg = TestFESpace(model,reffe_scalar;dirichlet_tags=["Gamma_N"])
+  U_reg = TrialFESpace(V_reg,0)
+
+  ## Create FE functions
+  φh = interpolate(gen_lsf(4,0.2),V_φ)
+
+  ## Interpolation and weak form
+  interp = SmoothErsatzMaterialInterpolation(η = η_coeff*maximum(Δ))
+  I,H,DH,ρ = interp.I,interp.H,interp.DH,interp.ρ
+
+  a(u,v,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*(C ⊙ ε(u) ⊙ ε(v)))dΩ
+  l(v,φ,dΩ,dΓ_N) = ∫(v⋅g)dΓ_N
+  res(u,v,φ,dΩ,dΓ_N) = a(u,v,φ,dΩ,dΓ_N) - l(v,φ,dΩ,dΓ_N)
+
+  ## Optimisation functionals
+  ξ = 0.25;
+  J(u,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*(C ⊙ ε(u) ⊙ ε(u)) + ξ*(ρ ∘ φ))dΩ
+  dJ(q,u,φ,dΩ,dΓ_N) = ∫((ξ - C ⊙ ε(u) ⊙ ε(u))*q*(DH ∘ φ)*(norm ∘ ∇(φ)))dΩ
+  Vol(u,φ,dΩ,dΓ_N) = ∫(((ρ ∘ φ) - 0.5)/vol_D)dΩ
+  dVol(q,u,φ,dΩ,dΓ_N) = ∫(1/vol_D*q*(DH ∘ φ)*(norm ∘ ∇(φ)))dΩ
+
+  ## Finite difference solver and level set function
+  stencil = AdvectionStencil(FirstOrderStencil(3,Float64),model,V_φ,tol,max_steps)
+  reinit!(stencil,φh,γ_reinit)
+
+  ## Setup solver and FE operators
+  Tm = SparseMatrixCSR{0,PetscScalar,PetscInt}
+  Tv = Vector{PetscScalar}
+  solver = ElasticitySolver(V)
+  
+  state_map = AffineFEStateMap(
+    a,l,U,V,V_φ,U_reg,φh,dΩ,dΓ_N;
+    assem_U = SparseMatrixAssembler(Tm,Tv,U,V),
+    assem_adjoint = SparseMatrixAssembler(Tm,Tv,V,U),
+    assem_deriv = SparseMatrixAssembler(Tm,Tv,U_reg,U_reg),
+    ls = solver, adjoint_ls = solver
+  )
+  pcfs = PDEConstrainedFunctionals(J,[Vol],state_map;analytic_dJ=dJ,analytic_dC=[dVol])
+
+  ## Hilbertian extension-regularisation problems
+  α = α_coeff*maximum(Δ)
+  a_hilb(p,q) = ∫(α^2*∇(p)⋅∇(q) + p*q)dΩ;
+  vel_ext = VelocityExtension(
+    a_hilb,U_reg,V_reg;
+    assem = SparseMatrixAssembler(Tm,Tv,U_reg,V_reg),
+    ls = PETScLinearSolver()
+  )
+
+  ## Optimiser
+  return AugmentedLagrangian(pcfs,stencil,vel_ext,φh;γ,γ_reinit,verbose=verbose)
+end
+
+with_mpi() do distribute
+  # Setup
+  mesh_partition = (N,N,N)
+  ranks = distribute(LinearIndices((prod(mesh_partition),)))
+  el_size = (N_EL,N_EL,N_EL)
+  verbose = Bool(VERBOSE) ? i_am_main(ranks) : false; 
+  if PROB_TYPE == "NLELAST"
+    options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true 
+      -ksp_converged_reason -ksp_rtol 1.0e-12 -mat_block_size 3
+      -mg_levels_ksp_type chebyshev -mg_levels_esteig_ksp_type cg -mg_coarse_sub_pc_type cholesky"
+    opt = nl_elast
+  elseif PROB_TYPE == "THERM" 
+    options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true 
+      -ksp_converged_reason -ksp_rtol 1.0e-12"
+    opt = therm
+  elseif PROB_TYPE == "ELAST"
+    options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true 
+      -ksp_converged_reason -ksp_rtol 1.0e-12 -mat_block_size 3
+      -mg_levels_ksp_type chebyshev -mg_levels_esteig_ksp_type cg -mg_coarse_sub_pc_type cholesky"
+    opt = elast
+  else
+    error("Problem not defined")
+  end
+
+  # Output
+  out_dir = "./results/benchmarks/"
+  i_am_main(ranks) && ~isdir(out_dir) ? mkpath(out_dir) : nothing
+  # Run
+  GridapPETSc.with(args=split(options)) do
+    optim = opt(mesh_partition,ranks,el_size,ORDER,VERBOSE)
+    ## Benchmark optimiser
+    bopt = benchmark_optimizer(optim, 1, nothing)
+    ## Benchmark forward problem
+    bfwd = benchmark_forward_problem(optim.problem.state_map, optim.φ0, nothing)
+    ## Benchmark advection
+    v = get_free_dof_values(interpolate(FEFunction(LSTO_Distributed.get_deriv_space(optim.problem.state_map),
+      optim.problem.dJ),LSTO_Distributed.get_aux_space(optim.problem.state_map)))
+    badv = benchmark_advection(optim.stencil, get_free_dof_values(optim.φ0), v, 0.1, nothing)
+    ## Benchmark reinitialisation
+    brinit = benchmark_reinitialisation(optim.stencil, get_free_dof_values(optim.φ0), 0.1, nothing)
+    ## Benchmark velocity extension
+    bvelext = benchmark_velocity_extension(optim.vel_ext, optim.problem.dJ, nothing)
+    if i_am_main(ranks)
+      open(out_dir*NAME*".txt","w") do f
+        bcontent = "bopt(1),bfwd,badv,brinit,bvelext\n$bopt,$bfwd,$badv,$brinit,$bvelext";
+        write(f,bcontent)
+      end
+    end
+  end
+end
---
+++ scripts/Benchmarks/generate_benchmark_scripts.jl
@@ -0,0 +1,95 @@
+using Mustache
+
+function generate(name,type;cputype="7702",wallhr=2,n_mesh_partition,
+    mem=8*n_mesh_partition^3,n_el_size,fe_order=1,verbose=1)
+
+  ncpus = n_mesh_partition^3
+
+  job_data = """
+  #!/bin/bash -l
+
+  #PBS -P LSTO
+  #PBS -N "{{:name}}"
+  #PBS -l cputype={{:cputype}}
+  #PBS -l ncpus={{:ncpus}}
+  #PBS -l mpiprocs={{:ncpus}}
+  #PBS -l mem={{:mem}}GB
+  #PBS -l walltime={{:wallhr}}:00:00
+  #PBS -j oe
+
+  cd \$PBS_O_WORKDIR
+
+  source \$HOME/hpc-environments-main/lyra/load-ompi.sh
+  export mpiexecjl=\$HOME/.julia/bin/mpiexecjl
+
+  julia --project=. -e "using Pkg; Pkg.precompile()"
+  
+  NAME="{{:name}}"
+  PROB_TYPE={{:type}}
+  N={{:n_mesh_partition}}
+  N_EL={{:n_el_size}}
+  ORDER={{:fe_order}}
+  VERBOSE={{:verbose}}
+
+  \$mpiexecjl --project=. -n {{:ncpus}} julia \\
+    --check-bounds no \\
+    ./scripts/Benchmarks/benchmark.jl \\
+    \$NAME \\
+    \$PROB_TYPE \\
+    \$N \\
+    \$N_EL \\
+    \$ORDER \\
+    \$VERBOSE
+  """;
+
+  settings = (;name,type,cputype,wallhr,ncpus,n_mesh_partition,n_el_size,mem,fe_order,verbose)
+  Mustache.render(job_data, settings)
+end
+
+mem_per_node = 1003;
+cpus_per_node = 128;
+gb_per_cpu = mem_per_node/cpus_per_node*(1-0.001);
+dofs_per_proc = 32000;
+
+function generate_jobs(phys_type,ndof_per_node)
+  N = vcat(1,2:2:16);
+  strong = (N).^3;
+  strong_el_x=128;
+  mem = ceil.(Int,strong*gb_per_cpu);
+  weak_el_x = @. floor(Int,(dofs_per_proc*strong/ndof_per_node)^(1/3)-1);
+
+  node_cpu_usage = strong/cpus_per_node
+  node_mem_usage = mem/mem_per_node
+  dof_sanity_check = maximum(abs,@. ndof_per_node*(weak_el_x+1)^3/strong/dofs_per_proc - 1)
+
+  sname(phys_type,ndof_per_node,n,elx) = "STRONG_$(phys_type)_dof=$(ndof_per_node)_N=$(n)_elx=$(elx)"
+  wname(phys_type,ndof_per_node,n,elx) = "WEAK_$(phys_type)_dof=$(ndof_per_node)_N=$(n)_elx=$(elx)"
+
+  strong_jobs = map(n->(sname(phys_type,ndof_per_node,n,strong_el_x),
+    generate((sname(phys_type,ndof_per_node,n,strong_el_x)),phys_type;
+    n_mesh_partition=n,n_el_size=strong_el_x)),N)
+  weak_jobs = map((n,elx)->(wname(phys_type,ndof_per_node,n,elx),
+    generate((wname(phys_type,ndof_per_node,n,elx)),phys_type;
+    n_mesh_partition=n,n_el_size=elx)),N,weak_el_x)
+
+  strong_jobs,weak_jobs,node_cpu_usage,node_mem_usage,dof_sanity_check
+end
+
+nlelas_strong_jobs,nlelas_weak_jobs,nlelas_node_cpu_usage,
+  nlelas_node_mem_usage,nlelas_dof_sanity_check = generate_jobs("NLELAST",3)
+
+elas_strong_jobs,elas_weak_jobs,elas_node_cpu_usage,
+  elas_node_mem_usage,elas_dof_sanity_check = generate_jobs("ELAST",3)
+
+ther_strong_jobs,ther_weak_jobs,ther_node_cpu_usage,
+  ther_node_mem_usage,ther_dof_sanity_check = generate_jobs("THERM",1)
+
+for job in vcat(nlelas_strong_jobs,nlelas_weak_jobs,
+    elas_strong_jobs,elas_weak_jobs,
+    ther_strong_jobs,ther_weak_jobs)
+  name = job[1]
+  content = job[2]
+  open("./scripts/Benchmarks/jobs/$name.pbs","w") do f
+    write(f,content)
+  end
+end
