1bf1c55d8c9983ed27f855ec51e4a283feefb73c
Z J Wegert (Workstation) <zach.wegert@hdr.qut.edu.au>
2024-01-17 10:30:16 +1000

    adjust

--- scripts/MPI/MPI_main_3d_hyperelastic_compliance.jl
+++ scripts/MPI/MPI_main_3d_hyperelastic_compliance.jl
@@ -117,7 +117,7 @@ function main(mesh_partition,distribute,el_size)
 end
 
 with_mpi() do distribute
-  mesh_partition = (7,5,5)
+  mesh_partition = (7,6,6)
   el_size = (200,100,100)
   hilb_solver_options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true 
     -ksp_converged_reason -ksp_rtol 1.0e-12 -mat_block_size 3
