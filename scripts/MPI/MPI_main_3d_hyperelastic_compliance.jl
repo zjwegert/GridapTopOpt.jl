339481f008734c917730edbe347374736908d098
Z J Wegert (Workstation) <zach.wegert@hdr.qut.edu.au>
2024-01-25 16:48:55 +1000

    Fix on Lyra

--- scripts/MPI/MPI_main_3d_hyperelastic_compliance.jl
+++ scripts/MPI/MPI_main_3d_hyperelastic_compliance.jl
@@ -117,8 +117,8 @@ function main(mesh_partition,distribute,el_size)
 end
 
 with_mpi() do distribute
-  mesh_partition = (3,2,2)
-  el_size = (81,41,41)
+  mesh_partition = (5,5,5)
+  el_size = (160,80,80)
   hilb_solver_options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true 
     -ksp_converged_reason -ksp_rtol 1.0e-12 -mat_block_size 3
     -mg_levels_ksp_type chebyshev -mg_levels_esteig_ksp_type cg -mg_coarse_sub_pc_type cholesky"
