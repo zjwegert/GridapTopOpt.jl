4b3f304c19583e78a3e3ee0122b34945679ffa26
Zach Wegert <zach.wegert@hdr.qut.edu.au>
2024-02-03 15:40:19 +1000

    VelocityExtension.jl docs & clean

--- src/VelocityExtension.jl
+++ src/VelocityExtension.jl
@@ -1,20 +1,37 @@
 """
-  struct VelocityExtension{A,B} end
+  struct VelocityExtension{A,B}
 
-Wrapper to hold stiffness matrix and cache for
-  Hilbertian extension-regularisation procedure.
+Wrapper to hold a stiffness matrix and a cache for
+the Hilbertian extension-regularisation. See Allaire et al.
+(10.1016/bs.hna.2020.10.004_978-0-444-64305-6_2021).
+
+# Properties
+
+- `K::A`
+- `cache::B`
 """
 struct VelocityExtension{A,B}
   K     :: A
   cache :: B
 end
 
+"""
+  VelocityExtension(biform,U_reg,V_reg;assem,ls)
+  
+Create an instance of `VelocityExtension` given a bilinear form `biform`,
+trial space `U_reg`, and test space `V_reg`.
+
+# Optional
+
+- `assem`: A matrix assembler
+- `ls::LinearSolver`: A linear solver
+"""
 function VelocityExtension(
-    biform,
-    U_reg,
-    V_reg;
+    biform::Function,
+    U_reg::FESpace,
+    V_reg::FESpace;
     assem = SparseMatrixAssembler(U_reg,V_reg),
-    ls = LUSolver())   
+    ls::LinearSolver = LUSolver())   
   ## Assembly
   K  = assemble_matrix(biform,assem,U_reg,V_reg)
   ns = numerical_setup(symbolic_setup(ls,K),K)
@@ -26,10 +43,8 @@ end
 """
   project!(vel_ext::VelocityExtension,dF::AbstractVector) -> dF
 
-Apply Hilbertian extension-regularisation to dF to project
- gradient into a space with additional regularity over the 
- bounding domain. See Allaire et al.
-  (10.1016/bs.hna.2020.10.004_978-0-444-64305-6_2021).
+Project shape derivative `dF` onto a function space described
+by the `vel_ext`.
 """
 function project!(vel_ext::VelocityExtension,dF::AbstractVector)
   ns, x = vel_ext.cache
@@ -39,4 +54,4 @@ function project!(vel_ext::VelocityExtension,dF::AbstractVector)
   return dF
 end
 
-project!(vel_ext::VelocityExtension,dF_vec::Vector{<:AbstractVector}) = map(dF -> project!(vel_ext,dF),dF_vec)
+project!(vel_ext::VelocityExtension,dF_vec::Vector{<:AbstractVector}) = broadcast(dF -> project!(vel_ext,dF),dF_vec)
