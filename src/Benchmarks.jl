0c6e80f5f1d3db9ecec0e69870b870cb3580b7e7
zjwegert <60646897+zjwegert@users.noreply.github.com>
2024-02-06 16:15:31 +1000

    Benchmarks.jl docs and small rearrange

--- src/Benchmarks.jl
+++ src/Benchmarks.jl
@@ -1,19 +1,22 @@
-function process_timer(t::PTimer)
-  data = t.data
-  map_main(data) do data
-    times = map(x -> x.max,values(data))
-    process_timer(times)
-  end |> PartitionedArrays.getany
-end
+"""
+  benchmark(f, args, ranks; nreps, reset!)
 
-function process_timer(t::Vector)
-  # return length(t), maximum(t), minimum(t), sum(t)/length(t)
-  return t
-end
+Benchmark a function `f` that takes arguments `args`. In MPI mode, 
+benchmark will always return the maximum CPU time across all ranks.
+This behaviour can be changed by overwritting `process_timer`.
 
+# Important
+The input `ranks` allows the user to provide the MPI ranks, `benchmark`
+will not function correctly in MPI mode if these are not supplied.
+
+# Optional
+
+- `nreps = 10`: Number of benchmark repetitions
+- `reset!= (x...) -> nothing`: Function for reseting inital data (e.g., level-set function φ). 
+"""
 function benchmark(f, args, ranks::Nothing; nreps = 10, reset! = (x...) -> nothing)
   t = zeros(Float64,nreps)
-  println("<------------- Compilation ------------->")
+  println("<------------- Compilation  ------------->")
   f(args...)
   println("<------------- Benchmarking ------------->")
   for i in 1:nreps
@@ -26,7 +29,7 @@ end
 
 function benchmark(f, args, ranks; nreps = 10, reset! = (x...) -> nothing)
   t = PTimer(ranks)
-  i_am_main(ranks) && println("<------------- Compilation ------------->")
+  i_am_main(ranks) && println("<------------- Compilation  ------------->")
   f(args...)
   i_am_main(ranks) && println("<------------- Benchmarking ------------->")
   for i in 1:nreps
@@ -39,6 +42,25 @@ function benchmark(f, args, ranks; nreps = 10, reset! = (x...) -> nothing)
   return process_timer(t)
 end
 
+function process_timer(t::Vector)
+  # return length(t), maximum(t), minimum(t), sum(t)/length(t)
+  return t
+end
+
+function process_timer(t::PTimer)
+  data = t.data
+  map_main(data) do data
+    times = map(x -> x.max,values(data))
+    process_timer(times)
+  end |> PartitionedArrays.getany
+end
+
+## Standard benchmarks
+"""
+  benchmark_optimizer(m::Optimiser, niter, ranks; nreps)
+
+Given an optimiser `m`, benchmark `niter` iterations.
+"""
 function benchmark_optimizer(m::Optimiser, niter, ranks; nreps = 10)
   function f(m)
     _, state = iterate(m)
@@ -55,6 +77,12 @@ function benchmark_optimizer(m::Optimiser, niter, ranks; nreps = 10)
   return benchmark(f, (m,), ranks; nreps, reset! = opt_reset!)
 end
 
+"""
+  benchmark_forward_problem(m::AbstractFEStateMap, φh, ranks; nreps)
+
+Benchmark the forward FE solve given `m::AbstractFEStateMap` and a level-set 
+function `φh`. See [`forward_solve`](@ref) for input types.
+"""
 function benchmark_forward_problem(m::AbstractFEStateMap, φh, ranks; nreps = 10)
   function f(m, φh)
     forward_solve(m,φh)
@@ -66,6 +94,13 @@ function benchmark_forward_problem(m::AbstractFEStateMap, φh, ranks; nreps = 10
   return benchmark(f, (m,φh), ranks; nreps)
 end
 
+"""
+  benchmark_advection(stencil::AdvectionStencil, φ0, v0, γ, ranks; nreps)
+
+Benchmark solving the Hamilton-Jacobi evolution equation given a `stencil`,
+level-set function `φ0`, velocity function `v0`, and time step coefficient `γ`. 
+See [`advect!`](@ref) for input types.
+"""
 function benchmark_advection(stencil::AdvectionStencil, φ0, v0, γ, ranks; nreps = 10)
   function f(stencil,φ,v,γ)
     advect!(stencil,φ,v,γ)
@@ -79,6 +114,12 @@ function benchmark_advection(stencil::AdvectionStencil, φ0, v0, γ, ranks; nrep
   return benchmark(f, (stencil,φ,v,γ), ranks; nreps, reset!)
 end
 
+"""
+  benchmark_reinitialisation(stencil::AdvectionStencil, φ0, γ_reinit, ranks; nreps)
+
+Benchmark solving the reinitialisation equation given a `stencil`, level-set function
+`φ0`, and time step coefficient `γ`. See [`reinit!`](@ref) for input types.
+"""
 function benchmark_reinitialisation(stencil::AdvectionStencil, φ0, γ_reinit, ranks; nreps = 10)
   function f(stencil,φ,γ_reinit)
     reinit!(stencil,φ,γ_reinit)
@@ -90,6 +131,12 @@ function benchmark_reinitialisation(stencil::AdvectionStencil, φ0, γ_reinit, r
   return benchmark(f, (stencil,φ,γ_reinit), ranks; nreps, reset!)
 end
 
+"""
+  benchmark_velocity_extension(ext::VelocityExtension, v0, ranks; nreps)
+
+Benchmark the Hilbertian velocity-extension method `ext` given a RHS `v0`.
+See [`project!`](@ref) for input types.
+"""
 function benchmark_velocity_extension(ext::VelocityExtension, v0, ranks; nreps = 10)
   function f(ext,v)
     project!(ext,v)
@@ -101,6 +148,13 @@ function benchmark_velocity_extension(ext::VelocityExtension, v0, ranks; nreps =
   return benchmark(f, (ext,v), ranks; nreps, reset!)
 end
 
+"""
+  benchmark_hilbertian_projection_map(m::HilbertianProjectionMap, dV, C, dC, K, ranks; nreps)
+
+Benchmark the `HilbertianProjectionMap` given a objective sensitivity `dV`, constraint values C,
+constraint sensitivities `dC`, and stiffness matrix `K` for the velocity-extension.
+See [`update_descent_direction!`](@ref) for input types.
+"""
 function benchmark_hilbertian_projection_map(m::HilbertianProjectionMap, dV, C, dC, K, ranks; nreps = 10)
   function f(m,dV,C,dC,K)
     update_descent_direction!(m,dV,C,dC,K)
