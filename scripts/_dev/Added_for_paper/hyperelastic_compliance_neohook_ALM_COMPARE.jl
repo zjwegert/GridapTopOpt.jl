5d4bc2eb1263a2ea29ad827e42afd69df3559a9f
zjwegert <60646897+zjwegert@users.noreply.github.com>
2024-02-23 20:11:29 +1000

    added for paper

---
+++ scripts/_dev/Added_for_paper/hyperelastic_compliance_neohook_ALM_COMPARE.jl
@@ -0,0 +1,193 @@
+using Gridap, LevelSetTopOpt
+
+"""
+  (Serial) Minimum hyperelastic (neohookean) compliance with augmented Lagrangian method in 2D.
+""" 
+function main()
+  ## Parameters
+  order = 1
+  xmax,ymax=2.0,1.0
+  prop_Γ_N = 0.4
+  dom = (0,xmax,0,ymax)
+  el_size = (400,200)
+  γ = 0.1
+  γ_reinit = 0.5
+  max_steps = floor(Int,minimum(el_size)/10)
+  tol = 1/(10order^2)*prod(inv,minimum(el_size))
+  η_coeff = 2
+  α_coeff = 4
+  vf = 0.4
+  path = dirname(dirname(@__DIR__))*"/results/highres_hyperelastic_compliance_neohook_ALM"
+  mkdir(path)
+
+  ## FE Setup
+  model = CartesianDiscreteModel(dom,el_size);
+  el_Δ = get_el_Δ(model)
+  f_Γ_D(x) = (x[1] ≈ 0.0)
+  f_Γ_N(x) = (x[1] ≈ xmax && ymax/2-ymax*prop_Γ_N/4 - eps() <= x[2] <= ymax/2+ymax*prop_Γ_N/4 + eps())
+  update_labels!(1,model,f_Γ_D,"Gamma_D")
+  update_labels!(2,model,f_Γ_N,"Gamma_N")
+
+  ## Triangulations and measures
+  Ω = Triangulation(model)
+  Γ_N = BoundaryTriangulation(model,tags="Gamma_N")
+  dΩ = Measure(Ω,2*order)
+  dΓ_N = Measure(Γ_N,2*order)
+  vol_D = sum(∫(1)dΩ)
+
+  ## Spaces
+  reffe = ReferenceFE(lagrangian,VectorValue{2,Float64},order)
+  reffe_scalar = ReferenceFE(lagrangian,Float64,order)
+  V = TestFESpace(model,reffe;dirichlet_tags=["Gamma_D"])
+  U = TrialFESpace(V,VectorValue(0.0,0.0))
+  V_φ = TestFESpace(model,reffe_scalar)
+  V_reg = TestFESpace(model,reffe_scalar;dirichlet_tags=["Gamma_N"])
+  U_reg = TrialFESpace(V_reg,0)
+
+  ## Create FE functions
+  φh = interpolate(initial_lsf(4,0.2),V_φ)
+
+  ## Interpolation and weak form
+  interp = SmoothErsatzMaterialInterpolation(η = η_coeff*maximum(el_Δ))
+  I,H,DH,ρ = interp.I,interp.H,interp.DH,interp.ρ
+
+  ## Material properties
+  _E = 1000
+  ν = 0.3
+  μ, λ = _E/(2*(1 + ν)), _E*ν/((1 + ν)*(1 - 2*ν))
+  g = VectorValue(0,-10)
+
+  ## Neohookean hyperelastic material
+  # Deformation gradient
+  F(∇u) = one(∇u) + ∇u'
+  J(F) = sqrt(det(C(F)))
+
+  # Derivative of green Strain
+  dE(∇du,∇u) = 0.5*( ∇du⋅F(∇u) + (∇du⋅F(∇u))' )
+
+  # Right Caughy-green deformation tensor
+  C(F) = (F')⋅F
+
+  # Constitutive law (Neo hookean)
+  function S(∇u)
+    Cinv = inv(C(F(∇u)))
+    μ*(one(∇u)-Cinv) + λ*log(J(F(∇u)))*Cinv
+  end
+
+  # Cauchy stress tensor and residual
+  σ(∇u) = (1.0/J(F(∇u)))*F(∇u)⋅S(∇u)⋅(F(∇u))'
+  res(u,v,φ,dΩ,dΓ_N) = ∫( (I ∘ φ)*((dE∘(∇(v),∇(u))) ⊙ (S∘∇(u))) )*dΩ - ∫(g⋅v)dΓ_N
+
+  ## Optimisation functionals
+  Obj(u,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*((dE∘(∇(u),∇(u))) ⊙ (S∘∇(u))))dΩ
+  Vol(u,φ,dΩ,dΓ_N) = ∫(((ρ ∘ φ) - vf)/vol_D)dΩ;
+  dVol(q,u,φ,dΩ,dΓ_N) = ∫(-1/vol_D*q*(DH ∘ φ)*(norm ∘ ∇(φ)))dΩ
+
+  ## Finite difference solver and level set function
+  stencil = AdvectionStencil(FirstOrderStencil(2,Float64),model,V_φ,tol,max_steps)
+
+  ## Setup solver and FE operators
+  state_map = NonlinearFEStateMap(res,U,V,V_φ,U_reg,φh,dΩ,dΓ_N)
+  pcfs = PDEConstrainedFunctionals(Obj,[Vol],state_map,analytic_dC=[dVol])
+
+  ## Hilbertian extension-regularisation problems
+  α = α_coeff*maximum(el_Δ)
+  a_hilb(p,q) =∫(α^2*∇(p)⋅∇(q) + p*q)dΩ
+  vel_ext = VelocityExtension(a_hilb,U_reg,V_reg)
+  
+  ## Optimiser
+  optimiser = AugmentedLagrangian(pcfs,stencil,vel_ext,φh;
+    γ,γ_reinit,verbose=true,constraint_names=[:Vol])
+  for (it,uh,φh) in optimiser
+    write_vtk(Ω,path*"/struc_$it",it,["phi"=>φh,"H(phi)"=>(H ∘ φh),"|nabla(phi)|"=>(norm ∘ ∇(φh)),"uh"=>uh])
+    write_history(path*"/history.txt",optimiser.history)
+  end
+  it = get_history(optimiser).niter; uh = get_state(pcfs)
+  write_vtk(Ω,path*"/struc_$it",it,["phi"=>φh,"H(phi)"=>(H ∘ φh),"|nabla(phi)|"=>(norm ∘ ∇(φh)),"uh"=>uh];iter_mod=1)
+end
+
+main()
+
+function main_LE_comparison()
+  ## Parameters
+  order = 1
+  xmax,ymax=(2.0,1.0)
+  prop_Γ_N = 0.4
+  dom = (0,xmax,0,ymax)
+  el_size = (400,200)
+  γ = 0.1
+  γ_reinit = 0.5
+  max_steps = floor(Int,minimum(el_size)/10)
+  tol = 1/(10order^2)*prod(inv,minimum(el_size))
+  C = isotropic_elast_tensor(2,1000,0.3)
+  η_coeff = 2
+  α_coeff = 4
+  vf = 0.4
+  g = VectorValue(0,-10)
+  path = dirname(dirname(@__DIR__))*"/results/highres_LE_comparison"
+  mkdir(path)
+
+  ## FE Setup
+  model = CartesianDiscreteModel(dom,el_size)
+  el_Δ = get_el_Δ(model)
+  f_Γ_D(x) = (x[1] ≈ 0.0)
+  f_Γ_N(x) = (x[1] ≈ xmax && ymax/2-ymax*prop_Γ_N/4 - eps() <= x[2] <= ymax/2+ymax*prop_Γ_N/4 + eps())
+  update_labels!(1,model,f_Γ_D,"Gamma_D")
+  update_labels!(2,model,f_Γ_N,"Gamma_N")
+
+  ## Triangulations and measures
+  Ω = Triangulation(model)
+  Γ_N = BoundaryTriangulation(model,tags="Gamma_N")
+  dΩ = Measure(Ω,2*order)
+  dΓ_N = Measure(Γ_N,2*order)
+  vol_D = sum(∫(1)dΩ)
+
+  ## Spaces
+  reffe = ReferenceFE(lagrangian,VectorValue{2,Float64},order)
+  reffe_scalar = ReferenceFE(lagrangian,Float64,order)
+  V = TestFESpace(model,reffe;dirichlet_tags=["Gamma_D"])
+  U = TrialFESpace(V,VectorValue(0.0,0.0))
+  V_φ = TestFESpace(model,reffe_scalar)
+  V_reg = TestFESpace(model,reffe_scalar;dirichlet_tags=["Gamma_N"])
+  U_reg = TrialFESpace(V_reg,0)
+
+  ## Create FE functions
+  φh = interpolate(initial_lsf(4,0.2),V_φ)
+
+  ## Interpolation and weak form
+  interp = SmoothErsatzMaterialInterpolation(η = η_coeff*maximum(el_Δ))
+  I,H,DH,ρ = interp.I,interp.H,interp.DH,interp.ρ
+
+  a(u,v,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*(C ⊙ ε(u) ⊙ ε(v)))dΩ
+  l(v,φ,dΩ,dΓ_N) = ∫(v⋅g)dΓ_N
+
+  ## Optimisation functionals
+  J(u,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*(C ⊙ ε(u) ⊙ ε(u)))dΩ
+  dJ(q,u,φ,dΩ,dΓ_N) = ∫((C ⊙ ε(u) ⊙ ε(u))*q*(DH ∘ φ)*(norm ∘ ∇(φ)))dΩ;
+  Vol(u,φ,dΩ,dΓ_N) = ∫(((ρ ∘ φ) - vf)/vol_D)dΩ;
+  dVol(q,u,φ,dΩ,dΓ_N) = ∫(-1/vol_D*q*(DH ∘ φ)*(norm ∘ ∇(φ)))dΩ
+
+  ## Finite difference solver and level set function
+  stencil = AdvectionStencil(FirstOrderStencil(2,Float64),model,V_φ,tol,max_steps)
+
+  ## Setup solver and FE operators
+  state_map = AffineFEStateMap(a,l,U,V,V_φ,U_reg,φh,dΩ,dΓ_N)
+  pcfs = PDEConstrainedFunctionals(J,[Vol],state_map,analytic_dJ=dJ,analytic_dC=[dVol])
+
+  ## Hilbertian extension-regularisation problems
+  α = α_coeff*maximum(el_Δ)
+  a_hilb(p,q) =∫(α^2*∇(p)⋅∇(q) + p*q)dΩ;
+  vel_ext = VelocityExtension(a_hilb,U_reg,V_reg)
+  
+  ## Optimiser
+  optimiser = AugmentedLagrangian(pcfs,stencil,vel_ext,φh;
+    γ,γ_reinit,verbose=true,constraint_names=[:Vol])
+  for (it,uh,φh) in optimiser
+    write_vtk(Ω,path*"/struc_$it",it,["phi"=>φh,"H(phi)"=>(H ∘ φh),"|nabla(phi)|"=>(norm ∘ ∇(φh)),"uh"=>uh])
+    write_history(path*"/history.txt",optimiser.history)
+  end
+  it = get_history(optimiser).niter; uh = get_state(pcfs)
+  write_vtk(Ω,path*"/struc_$it",it,["phi"=>φh,"H(phi)"=>(H ∘ φh),"|nabla(phi)|"=>(norm ∘ ∇(φh)),"uh"=>uh];iter_mod=1)
+end
+
+main_LE_comparison()
