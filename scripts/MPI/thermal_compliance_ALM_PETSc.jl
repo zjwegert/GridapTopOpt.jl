0d3f7df28dfd1806bc29481c32f3f6b98e27b547
Z J Wegert (Workstation) <zach.wegert@hdr.qut.edu.au>
2024-02-23 16:05:56 +1000

    Added 2d thermal MPI + PETSc for paper

---
+++ scripts/MPI/thermal_compliance_ALM_PETSc.jl
@@ -0,0 +1,119 @@
+using Gridap, GridapDistributed, GridapPETSc, GridapSolvers, 
+  PartitionedArrays, LevelSetTopOpt, SparseMatricesCSR
+
+"""
+  (MPI) Minimum thermal compliance with augmented Lagrangian method in 2D.
+
+  Optimisation problem:
+      Min J(Ω) = ∫ κ*∇(u)⋅∇(u) dΩ
+      Ω
+    s.t., Vol(Ω) = vf,
+          ⎡u∈V=H¹(Ω;u(Γ_D)=0),
+          ⎣∫ κ*∇(u)⋅∇(v) dΩ = ∫ v dΓ_N, ∀v∈V.
+"""
+function main(mesh_partition,distribute,el_size)
+  ranks = distribute(LinearIndices((prod(mesh_partition),)))
+
+  ## Parameters
+  order = 1
+  xmax=ymax=1.0
+  prop_Γ_N = 0.4
+  prop_Γ_D = 0.2
+  dom = (0,xmax,0,ymax)
+  γ = 0.1
+  γ_reinit = 0.5
+  max_steps = floor(Int,minimum(el_size)/10)
+  tol = 1/(10order^2)*prod(inv,minimum(el_size))
+  κ = 1
+  η_coeff = 2
+  α_coeff = 4
+  vf = 0.4
+  path = dirname(dirname(@__DIR__))*"/results/thermal_compliance_ALM_PETSc"
+  i_am_main(ranks) && mkdir(path)
+
+  ## FE Setup
+  model = CartesianDiscreteModel(ranks,mesh_partition,dom,el_size);
+  el_Δ = get_el_Δ(model)
+  f_Γ_D(x) = (x[1] ≈ 0.0) && ((x[2] <= ymax*prop_Γ_D + eps()) || (x[2] >= ymax-ymax*prop_Γ_D - eps()))
+  f_Γ_N(x) = (x[1] ≈ xmax) && (ymax/2-ymax*prop_Γ_N/4 - eps() <= x[2] <= ymax/2+ymax*prop_Γ_N/4 + eps())
+  update_labels!(1,model,f_Γ_D,"Gamma_D")
+  update_labels!(2,model,f_Γ_N,"Gamma_N")
+
+  ## Triangulations and measures
+  Ω = Triangulation(model)
+  Γ_N = BoundaryTriangulation(model,tags="Gamma_N")
+  dΩ = Measure(Ω,2order)
+  dΓ_N = Measure(Γ_N,2order)
+  vol_D = sum(∫(1)dΩ)
+
+  ## Spaces
+  reffe_scalar = ReferenceFE(lagrangian,Float64,order)
+  V = TestFESpace(model,reffe_scalar;dirichlet_tags=["Gamma_D"])
+  U = TrialFESpace(V,0.0)
+  V_φ = TestFESpace(model,reffe_scalar)
+  V_reg = TestFESpace(model,reffe_scalar;dirichlet_tags=["Gamma_N"])
+  U_reg = TrialFESpace(V_reg,0)
+
+  ## Create FE functions
+  φh = interpolate(initial_lsf(4,0.2),V_φ)
+
+  ## Interpolation and weak form
+  interp = SmoothErsatzMaterialInterpolation(η = η_coeff*maximum(el_Δ))
+  I,H,DH,ρ = interp.I,interp.H,interp.DH,interp.ρ
+
+  a(u,v,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*κ*∇(u)⋅∇(v))dΩ
+  l(v,φ,dΩ,dΓ_N) = ∫(v)dΓ_N
+
+  ## Optimisation functionals
+  J(u,φ,dΩ,dΓ_N) = ∫((I ∘ φ)*κ*∇(u)⋅∇(u))dΩ
+  dJ(q,u,φ,dΩ,dΓ_N) = ∫(κ*∇(u)⋅∇(u)*q*(DH ∘ φ)*(norm ∘ ∇(φ)))dΩ;
+  Vol(u,φ,dΩ,dΓ_N) = ∫(((ρ ∘ φ) - vf)/vol_D)dΩ;
+  dVol(q,u,φ,dΩ,dΓ_N) = ∫(-1/vol_D*q*(DH ∘ φ)*(norm ∘ ∇(φ)))dΩ
+
+  ## Finite difference solver and level set function
+  stencil = AdvectionStencil(FirstOrderStencil(2,Float64),model,V_φ,tol,max_steps)
+
+  ## Setup solver and FE operators
+  Tm = SparseMatrixCSR{0,PetscScalar,PetscInt}
+  Tv = Vector{PetscScalar}
+  solver = PETScLinearSolver()
+  
+  state_map = AffineFEStateMap(
+    a,l,U,V,V_φ,U_reg,φh,dΩ,dΓ_N;
+    assem_U = SparseMatrixAssembler(Tm,Tv,U,V),
+    assem_adjoint = SparseMatrixAssembler(Tm,Tv,V,U),
+    assem_deriv = SparseMatrixAssembler(Tm,Tv,U_reg,U_reg),
+    ls = solver,adjoint_ls = solver
+  )
+  pcfs = PDEConstrainedFunctionals(J,[Vol],state_map,analytic_dJ=dJ)
+
+  ## Hilbertian extension-regularisation problems
+  α = α_coeff*maximum(el_Δ)
+  a_hilb(p,q) = ∫(α^2*∇(p)⋅∇(q) + p*q)dΩ;
+  vel_ext = VelocityExtension(
+    a_hilb, U_reg, V_reg;
+    assem = SparseMatrixAssembler(Tm,Tv,U_reg,V_reg),
+    ls = solver
+  )
+
+  ## Optimiser
+  optimiser = AugmentedLagrangian(pcfs,stencil,vel_ext,φh;γ,γ_reinit,
+    verbose=i_am_main(ranks),constraint_names=[:Vol])
+  for (it, uh, φh) in optimiser
+    write_vtk(Ω,path*"/struc_$it",it,["phi"=>φh,"H(phi)"=>(H ∘ φh),"|nabla(phi)|"=>(norm ∘ ∇(φh)),"uh"=>uh])
+    write_history(path*"/history.txt",optimiser.history;ranks=ranks)
+  end
+  it = get_history(optimiser).niter; uh = get_state(pcfs)
+  write_vtk(Ω,path*"/struc_$it",it,["phi"=>φh,"H(phi)"=>(H ∘ φh),"|nabla(phi)|"=>(norm ∘ ∇(φh)),"uh"=>uh];iter_mod=1)
+end
+
+with_mpi() do distribute
+  mesh_partition = (2,3)
+  el_size = (400,400)
+  solver_options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true 
+    -ksp_converged_reason -ksp_rtol 1.0e-12"
+  
+  GridapPETSc.with(args=split(all_solver_options)) do
+    main(mesh_partition,distribute,el_size)
+  end
+end
