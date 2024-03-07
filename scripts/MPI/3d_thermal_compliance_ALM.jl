7626979cfb19d3806a165f63796484e6eb8fae96
Z J Wegert (Workstation) <zach.wegert@hdr.qut.edu.au>
2024-03-08 08:43:41 +1000

    file naming for testing

--- scripts/MPI/3d_thermal_compliance_ALM.jl
+++ scripts/MPI/3d_thermal_compliance_ALM.jl
@@ -37,7 +37,7 @@ function main(mesh_partition,distribute,el_size,coef,step)
   η_coeff = 2
   α_coeff = 4
   vf = 0.4
-  path = dirname(dirname(@__DIR__))*"/results/3d_thermal_compliance_ALM_Coef$(coef)_Step$(step)"
+  path = dirname(dirname(@__DIR__))*"/results/3d_thermal_compliance_ALM_Nx$(el_size[1])_Coef$(coef)_Step$(step)"
   i_am_main(ranks) && mkdir(path)
 
   ## FE Setup
