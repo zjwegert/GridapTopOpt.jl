f0867462d190934f1c2884801f8463571737321e
JordiManyer <jordi.manyer@monash.edu>
2024-04-02 13:08:15 +1100

    Reworked VelocityExtension

--- src/VelocityExtension.jl
+++ src/VelocityExtension.jl
@@ -23,16 +23,22 @@ This provides two benefits:
 - `K::A`: The discretised inner product over ``H``.
 - `cache::B`: Cached objects used for [`project!`](@ref)
 """
-struct VelocityExtension{A,B}
+struct VelocityExtension{A,B,C,D}
   K     :: A
-  cache :: B
+  U_reg :: B
+  U_φ   :: C
+  rhs   :: D
+  cache :: E
 end
 
 """
-    VelocityExtension(biform,U_reg,V_reg;assem,ls)
+    VelocityExtension(lhs,rhs,U_reg,V_reg,U_φ;assem,ls)
   
-Create an instance of `VelocityExtension` given a bilinear form `biform`,
-trial space `U_reg`, and test space `V_reg`.
+# Arguments
+- `lhs`: The bilinear form of the identification problem.
+- `rhs`: The right-hand side of the identification problem.
+- `U_reg`: The Hilbertian extension-regularisation spaces.
+- `U_φ`: The non-regularised shape sensitivity space.
 
 # Optional
 
@@ -40,35 +46,84 @@ trial space `U_reg`, and test space `V_reg`.
 - `ls::LinearSolver`: A linear solver
 """
 function VelocityExtension(
-    biform::Function,
+    lhs::Function,
+    rhs::Function,
     U_reg::FESpace,
-    V_reg::FESpace;
-    assem = SparseMatrixAssembler(U_reg,V_reg),
-    ls::LinearSolver = LUSolver())   
+    U_φ::FESpace;
+    assem = SparseMatrixAssembler(U_reg,U_reg),
+    ls::LinearSolver = LUSolver()
+)
   ## Assembly
-  K  = assemble_matrix(biform,assem,U_reg,V_reg)
+  K  = assemble_matrix(lhs,assem,U_reg,U_reg)
+  cache = velocity_extension_caches(K,ls)
+  return VelocityExtension(K,U_reg,U_φ,rhs,cache)
+end
+
+function velocity_extension_caches(K::AbstractMatrix,ls)
   ns = numerical_setup(symbolic_setup(ls,K),K)
-  x  = allocate_in_domain(K)
-  cache = (ns,x)
-  return VelocityExtension(K,cache)
+  b  = allocate_in_range(K); fill!(b,zero(eltype(b)))
+  return (ns,b)
 end
 
-"""
-    project!(vel_ext::VelocityExtension,dF::AbstractVector) -> dF
-
-Project shape derivative `dF` onto a function space described
-by the `vel_ext`.
-"""
-function project!(vel_ext::VelocityExtension,dF::AbstractVector)
-  ns, x = vel_ext.cache
-  fill!(x,zero(eltype(x)))
-  solve!(x,ns,dF)
-  copy!(dF,x)
-  return dF
+function velocity_extension_caches(K::PSparseMatrix,ls)
+  ns = numerical_setup(symbolic_setup(ls,K),K)
+  x  = allocate_in_domain(K); fill!(x,zero(eltype(x)))
+  b  = allocate_in_range(K); fill!(b,zero(eltype(b)))
+  return (ns,x,b)
 end
-
-project!(vel_ext::VelocityExtension,dF_vec::Vector{<:AbstractVector}) = broadcast(dF -> project!(vel_ext,dF),dF_vec)
 
 function Base.show(io::IO,object::VelocityExtension)
   print(io,"$(nameof(typeof(object)))")
+end
+
+"""
+    project!(φh_reg::FEFunction,P::VelocityExtension,φh::FEFunction)
+    project!(φ_reg::AbstractVector,P::VelocityExtension,φ::AbstractVector)
+  
+  Projects the shape sensitivity `φh` into the Hilbertian extension-regularisation
+  space `U_reg`. Stores the result in `φh_reg`.
+"""
+function project!(φh_reg,P::VelocityExtension,φh)
+  φ_reg = get_free_dof_values(φh_reg)
+  project!(φ_reg,P,φh)
+  return φh_reg
+end
+
+function project!(φ_reg::AbstractVector,P::VelocityExtension,φ::AbstractVector)
+  φh = FEFunction(P.U_φ,φ)
+  project!(φ_reg,P,φh)
+  return φ_reg
+end
+
+function project!(φ_reg::AbstractVector,P::VelocityExtension{<:AbstractMatrix},φh)
+  ns, b = P.cache
+  assemble_vector!(v->P.rhs(φh,v),b,assem,V_reg)
+  solve!(φ_reg,ns,b)
+  return φ_reg
+end
+
+function project!(φ_reg::PVector,P::VelocityExtension{<:PsparseMatrix},φh)
+  ns, x, b = P.cache
+  assemble_vector!(v->P.rhs(φh,v),b,assem,V_reg)
+  solve!(x,ns,b)
+  copy!(φ_reg,x)
+  consistent!(φ_reg)
+  return φ_reg
+end
+
+"""
+    project(P::VelocityExtension,φh::FEFunction) -> FEFunction
+    project(P::VelocityExtension,φ::AbstractVector) -> AbstractVector
+  
+  Projects the shape sensitivity `φh` into the Hilbertian extension-regularisation
+  space `U_reg`. Allocates output.
+"""
+function project(P::VelocityExtension,φh)
+  φh_reg = zero(P.U_reg)
+  return project!(φh_reg,P,φh)
+end
+
+function project(P::VelocityExtension,φ::AbstractVector)
+  φ_reg = allocate_in_domain(P.K); fill!(φ_reg,zero(eltype(φ_reg)))
+  return project!(φ_reg,P,φ)
 end
