11bcd78b391ab49e8bd3f1b5da5492aeba6b585e
Z J Wegert <60646897+zjwegert@users.noreply.github.com>
2024-06-07 11:58:16 +1000

    testing

--- scripts/Embedded/gridap_embedded_testing_MWE_periodic.jl
+++ scripts/Embedded/gridap_embedded_testing_MWE_periodic.jl
@@ -8,10 +8,14 @@ model = CartesianDiscreteModel((0,1,0,1),(100,100),isperiodic=(true,true));
 V_φ = TestFESpace(model,ReferenceFE(lagrangian,Float64,1))
 
 φh = interpolate(x->-cos(4π*x[1])*cos(4*pi*x[2])/4-0.2/4,V_φ)
-point_to_coords = model.grid_topology.vertex_coordinates
-geo = DiscreteGeometry(get_free_dof_values(φh),point_to_coords,name="")
-cutgeo = cut(model,geo)
+analytic_geo=GridapEmbedded.disk(0.6;x0=Point(0.5,0.5))
+cutgeo = cut(model,analytic_geo)
+
+# point_to_coords = model.grid_topology.vertex_coordinates
+# geo = DiscreteGeometry(get_free_dof_values(φh),point_to_coords,name="")
+# cutgeo = cut(model,geo)
 
 Ω = Triangulation(cutgeo,PHYSICAL)
 
-writevtk(Ω,"./results/discrete_geo_serial")
+writevtk(Ω,"./results/discrete_geo_periodic",cellfields=["φh"=>φh])
+writevtk(Triangulation(model),"./results/discrete_geo_periodic_full",cellfields=["φh"=>φh])
