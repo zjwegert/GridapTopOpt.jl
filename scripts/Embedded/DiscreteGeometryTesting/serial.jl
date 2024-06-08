5e319d8fdfd41cf5f3a6bc2047b8f0c2ad3e5a0b
zjwegert <60646897+zjwegert@users.noreply.github.com>
2024-06-08 11:18:43 +1000

    Distributed compat with GridapEmbedded + periodic mesh

--- scripts/Embedded/DiscreteGeometryTesting/serial.jl
+++ scripts/Embedded/DiscreteGeometryTesting/serial.jl
@@ -1,11 +1,9 @@
-using Pkg; Pkg.activate()
-
 using Gridap
-using GridapEmbedded
-
+using GridapEmbedded,GridapEmbedded.LevelSetCutters
+import Gridap.Geometry: get_node_coordinates,collect1d
 
 model = CartesianDiscreteModel((0,1,0,1),(100,100));
-V_φ = TestFESpace(model,ReferenceFE(lagrangian,Float64,1))
+V_φ = TestFESpace(model,ReferenceFE(lagrangian,Float64,2))
 
 φh = interpolate(x->-cos(4π*x[1])*cos(4*pi*x[2])/4-0.2/4,V_φ)
 point_to_coords = collect1d(get_node_coordinates(model))
--- scripts/Embedded/gridap_embedded_testing_MWE_periodic.jl
+++ scripts/Embedded/gridap_embedded_testing_MWE_periodic.jl
@@ -7,15 +7,28 @@ import Gridap.Geometry: get_node_coordinates,collect1d
 model = CartesianDiscreteModel((0,1,0,1),(100,100),isperiodic=(true,true));
 V_φ = TestFESpace(model,ReferenceFE(lagrangian,Float64,1))
 
-φh = interpolate(x->-cos(4π*x[1])*cos(4*pi*x[2])/4-0.2/4,V_φ)
-analytic_geo=GridapEmbedded.disk(0.6;x0=Point(0.5,0.5))
-cutgeo = cut(model,analytic_geo)
-
-# point_to_coords = model.grid_topology.vertex_coordinates
-# geo = DiscreteGeometry(get_free_dof_values(φh),point_to_coords,name="")
-# cutgeo = cut(model,geo)
+## Discrete
+φh = interpolate(x->-cos(4π*(x[1]))*cos(4*pi*x[2])/4-0.2/4,V_φ)
+point_to_coords = collect1d(get_node_coordinates(model))
+geo = DiscreteGeometry(φh(point_to_coords),point_to_coords,name="")
+cutgeo = cut(model,geo)
 
 Ω = Triangulation(cutgeo,PHYSICAL)
+Ω_act = Triangulation(cutgeo,ACTIVE)
 
-writevtk(Ω,"./results/discrete_geo_periodic",cellfields=["φh"=>φh])
-writevtk(Triangulation(model),"./results/discrete_geo_periodic_full",cellfields=["φh"=>φh])
+# writevtk(Ω,"./results/discrete_geo_periodic",cellfields=["φh"=>φh])
+
+V_φ_test = TestFESpace(Ω_act,ReferenceFE(lagrangian,Float64,1))
+φh_test = interpolate(x->sqrt(x[1]^2+x[2]^2)-0.5,V_φ_test)
+writevtk(Ω,"./results/discrete_geo_periodic",cellfields=["φh"=>φh_test])
+
+## Analytic
+analytic_geo=AnalyticalGeometry(x->-cos(4π*(x[1]))*cos(4*pi*x[2])/4-0.2/4)
+cutgeo_analytic = cut(model,analytic_geo)
+Ω_analytic = Triangulation(cutgeo_analytic,PHYSICAL)
+Ω_analytic_act = Triangulation(cutgeo_analytic,ACTIVE)
+# writevtk(Ω_analytic,"./results/analytic_geo_periodic",cellfields=["φh"=>φh])
+
+V_φ_test = TestFESpace(Ω_analytic_act,ReferenceFE(lagrangian,Float64,1))
+φh_test = interpolate(x->sqrt(x[1]^2+x[2]^2)-0.5,V_φ_test)
+writevtk(Ω_analytic,"./results/analytic_geo_periodic",cellfields=["φh"=>φh_test])
