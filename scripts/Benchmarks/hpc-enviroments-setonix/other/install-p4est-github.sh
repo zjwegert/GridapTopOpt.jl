c7122d80b79e5ff14a44f7bbe021e155a66e0f18
zjwegert <60646897+zjwegert@users.noreply.github.com>
2024-07-05 10:29:59 +1000

    update install-p4est-github

--- scripts/Benchmarks/hpc-enviroments-setonix/other/install-p4est-github.sh
+++ scripts/Benchmarks/hpc-enviroments-setonix/other/install-p4est-github.sh
@@ -1,18 +1,18 @@
-# This script is specifically to build P4est from Github sources. 
-# This is necessary when installing P4est 2.2 with newer versions of Intel compilers. 
-# 
-# Steps: 
-#   - The key is that we require a newer version of autotools, which is unfortunately 
-#     not available in Gadi. 
+# This script is specifically to build P4est from Github sources.
+# This is necessary when installing P4est 2.2 with newer versions of Intel compilers.
+#
+# Steps:
+#   - The key is that we require a newer version of autotools, which is unfortunately
+#     not available in Gadi.
 #     Install instructions [here](https://docs.open-mpi.org/en/v5.0.x/developers/gnu-autotools.html).
 #   - Source the typical modules for IntelMPI.
-#   - Add the newly installed libraries to your path, i.e run 
+#   - Add the newly installed libraries to your path, i.e run
 #        `export PATH=/path-to-autotools/local/bin:$PATH`
 #   - Run this script
 
 CURR_DIR=$(pwd)
 PACKAGE=p4est
-INSTALL_ROOT=$HOME/bin
+INSTALL_ROOT=$MYSOFTWARE
 P4EST_INSTALL=$INSTALL_ROOT/$PACKAGE/$P4EST_VERSION-$MPI_VERSION
 ROOT_DIR=$INSTALL_ROOT/tmp
 SOURCES_DIR=$ROOT_DIR/$PACKAGE-$P4EST_VERSION
