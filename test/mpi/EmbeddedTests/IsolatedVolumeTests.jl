29f63347553f07acc56e808dfcf8f29a3727ca67
zjwegert <60646897+zjwegert@users.noreply.github.com>
2025-01-11 08:22:51 +1000

    Add test for MPI isolated volumes

--- test/mpi/EmbeddedTests/IsolatedVolumeTests.jl
+++ test/mpi/EmbeddedTests/IsolatedVolumeTests.jl
@@ -1,4 +1,4 @@
-# module IsolatedVolumeTestsMPI
+module IsolatedVolumeTestsMPI
 using Test
 using GridapTopOpt
 using Gridap
@@ -10,72 +10,21 @@ using Gridap.Geometry, Gridap.FESpaces, Gridap.CellData, Gridap.Adaptivity, Grid
 
 using GridapDistributed, PartitionedArrays
 
-function generate_model(D,n)
-  domain = (D==2) ? (0,1,0,1) : (0,1,0,1,0,1)
-  cell_partition = (D==2) ? (n,n) : (n,n,n)
-  base_model = UnstructuredDiscreteModel(CartesianDiscreteModel(domain,cell_partition))
-  ref_model = refine(base_model, refinement_method = "barycentric")
-  model = Adaptivity.get_model(ref_model)
-  update_labels!(1,model,x->x[1]≈0,"Gamma_D")
-  return model
-end
-
 function generate_model(D,n,ranks,mesh_partition)
   domain = (D==2) ? (0,1,0,1) : (0,1,0,1,0,1)
   cell_partition = (D==2) ? (n,n) : (n,n,n)
   base_model = UnstructuredDiscreteModel(CartesianDiscreteModel(ranks,mesh_partition,domain,cell_partition))
   ref_model = refine(base_model, refinement_method = "barycentric")
   model = Adaptivity.get_model(ref_model)
-  update_labels!(1,model,x->x[1]≈0,"Gamma_D")
   return model
 end
 
 function main_2d(model,name;vtk=false)
   order = 1
+  Ω = Triangulation(model)
 
-  reffe_scalar = ReferenceFE(lagrangian,Float64,order)
-  V_φ = TestFESpace(model,reffe_scalar)
-
-  f(x,y0) = abs(x[2]-y0) - 0.05
-  g(x,x0,y0,r) = sqrt((x[1]-x0)^2 + (x[2]-y0)^2) - r
-  f(x) = min(f(x,0.75),f(x,0.25),
-    g(x,0.15,0.5,0.1),
-    g(x,0.5,0.6,0.2),
-    g(x,0.85,0.5,0.1),
-    g(x,0.5,0.15,0.05))
-  φh = interpolate(f,V_φ)
-
-  geo = DiscreteGeometry(φh,model)
-  cutgeo = cut(model,geo)
-
-  bgcell_to_inoutcut = compute_bgcell_to_inoutcut(cutgeo,geo)
-  cell_to_color, color_to_group = GridapTopOpt.tag_isolated_volumes(model,bgcell_to_inoutcut;groups=((GridapTopOpt.CUT,IN),OUT))
-
-  μ = GridapTopOpt.get_isolated_volumes_mask(cutgeo,["Gamma_D"])
-
-  if vtk
-    writevtk(
-      Triangulation(model),"results/background_$name",
-      cellfields=[
-        "φh"=>φh,
-        "μ"=>μ,
-      ],
-      celldata=[
-        "inoutcut"=>bgcell_to_inoutcut,
-        "volumes"=>cell_to_color,
-      ];
-      append=false
-    )
-  end
-
-  return μ
-end
-
-function main_2d_distributed(model,name;vtk=false)
-  order = 1
-
-  reffe_scalar = ReferenceFE(lagrangian,Float64,order)
-  V_φ = TestFESpace(model,reffe_scalar)
+  reffe = ReferenceFE(lagrangian,Float64,order)
+  V_φ = TestFESpace(model,reffe)
 
   f(x,y0) = abs(x[2]-y0) - 0.05
   g(x,x0,y0,r) = sqrt((x[1]-x0)^2 + (x[2]-y0)^2) - r
@@ -89,63 +38,70 @@ function main_2d_distributed(model,name;vtk=false)
   geo = DiscreteGeometry(φh,model)
   cutgeo = cut(model,geo)
 
-  bgcell_to_inoutcut = compute_bgcell_to_inoutcut(cutgeo,geo)
-  cell_to_color, color_to_group = GridapTopOpt.tag_isolated_volumes(model,bgcell_to_inoutcut;groups=((GridapTopOpt.CUT,IN),OUT))
+  cell_to_state = map(compute_bgcell_to_inoutcut,local_views(model),local_views(geo))
+  cell_to_lcolor, lcolor_to_group = map(local_views(model),cell_to_state) do model, cell_to_state
+    GridapTopOpt.tag_isolated_volumes(model,cell_to_state;groups=((GridapTopOpt.CUT,IN),OUT))
+  end |> tuple_of_arrays;
 
-  μ = GridapTopOpt.get_isolated_volumes_mask(cutgeo,["Gamma_D"])
+  n_lcolor = map(length,lcolor_to_group)
+  cell_ids = partition(get_cell_gids(model))
 
-  _data = map(local_views(bgcell_to_inoutcut),local_views(model)) do bgcell_to_inoutcut,model
-    CellField(bgcell_to_inoutcut,Triangulation(model))
+  color_gids = GridapTopOpt.generate_volume_gids(
+    cell_ids,n_lcolor,cell_to_lcolor
+  )
+
+  cell_to_lcolor, lcolor_to_group, color_gids = GridapTopOpt.tag_isolated_volumes(model,cell_to_state;groups=((GridapTopOpt.CUT,IN),OUT));
+
+  μ = GridapTopOpt.get_isolated_volumes_mask(cutgeo,[1,2,5,7])
+
+  cell_to_color = map(cell_to_lcolor,partition(color_gids)) do cell_to_lcolor, colors
+    local_to_global(colors)[cell_to_lcolor]
   end
-  bgcell_to_inoutcut_field = GridapDistributed.DistributedCellField(_data,Triangulation(model))
-
-  _data_cell_to_color = map(local_views(cell_to_color),local_views(model)) do cell_to_color,model
-    CellField(cell_to_color,Triangulation(model))
-  end
-  cell_to_color_field = GridapDistributed.DistributedCellField(_data_cell_to_color,Triangulation(model))
 
   if vtk
+    Ω_φ = get_triangulation(V_φ)
     writevtk(
-      Triangulation(model),"results/background_$name",
+      Ω,"results/background_$name",
       cellfields=[
         "φh"=>φh,
         "μ"=>μ,
-        "inoutcut"=>bgcell_to_inoutcut_field,
-        "volumes"=>cell_to_color_field,
+        "inoutcut"=>CellField(cell_to_state,Ω_φ),
+        "loc_vols"=>CellField(cell_to_lcolor,Ω_φ),
+        "vols"=>CellField(cell_to_color,Ω_φ),
       ],
       append=false
-    )
+    );
   end
 
-  return μ
+  return μ,V_φ
 end
 
 function main(distribute,mesh_partition;vtk=false)
-  # Serial
-  serial_model = generate_model(2,40)
-  μ_serial = main_2d(serial_model,"serial";vtk)
-  μ_serial_val = getfield.(μ_serial.cell_field,:value)
-
-  # Distributed
   ranks = distribute(LinearIndices((prod(mesh_partition),)))
   model = generate_model(2,40,ranks,mesh_partition)
 
-  μ = main_2d_distributed(model,"distributed";vtk)
-  μ_data = map(local_views(μ)) do μ
-    getfield.(μ.cell_field,:value)
-  end
-  μ_pvec = PVector(μ_data,partition(get_face_gids(model,2)))
+  μ,V_φ = main_2d(model,"distributed";vtk)
 
-  @test μ_serial_val == collect(μ_pvec)
+  g(x,x0,y0,r) = sqrt((x[1]-x0)^2 + (x[2]-y0)^2) - r
+  f(x) = min(g(x,0.15,0.5,0.1),g(x,0.85,0.5,0.1))
+  fh = interpolate(f,V_φ)
+  geo = DiscreteGeometry(fh,model)
+  cell_to_state = map(compute_bgcell_to_inoutcut,local_views(model),local_views(geo))
 
-  if vtk
-    writevtk(Triangulation(serial_model),"iso_serial",cellfields=["χ_s"=>μ_serial]);
-    writevtk(Triangulation(model),"iso_dist",cellfields=["χ_s"=>μ]);
+  map(local_views(μ),cell_to_state) do μ,cell_to_state
+    @test getfield.(μ.cell_field,:value)==collect(cell_to_state.<=0)
   end
+end
+
+with_mpi() do distribute
+  main(distribute,(2,2);vtk=false)
+  main(distribute,(1,4);vtk=false)
+  main(distribute,(4,1);vtk=false)
 end
 
 with_debug() do distribute
-  @test main(distribute,(2,2);vtk=true)
+  main(distribute,(6,10);vtk=false)
+  main(distribute,(7,3);vtk=false)
 end
 
-# end
+end
