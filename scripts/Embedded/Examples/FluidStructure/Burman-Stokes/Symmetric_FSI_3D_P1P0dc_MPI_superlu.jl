8ba1f166b604f9fb5ba963dbc719f49ef4e0150a
zjwegert <60646897+zjwegert@users.noreply.github.com>
2025-04-02 09:59:53 +1000

    minor

--- scripts/Embedded/Examples/FluidStructure/Burman-Stokes/Symmetric_FSI_3D_P1P0dc_MPI_superlu.jl
+++ scripts/Embedded/Examples/FluidStructure/Burman-Stokes/Symmetric_FSI_3D_P1P0dc_MPI_superlu.jl
@@ -52,7 +52,7 @@ function main(ranks)
   max_steps = 20 # Based on number of elements in vertical direction divided by 10
   vf = 0.025
   α_coeff = γ_evo*max_steps
-  iter_mod = 1
+  iter_mod = 10
   D = 3
 
   # Load gmsh mesh (Currently need to update mesh.geo and these values concurrently)
