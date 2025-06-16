632167821fbbabeac6af56a7d69c085c3e3fc3e7
Z J Wegert (Workstation) <zach.wegert@hdr.qut.edu.au>
2025-06-16 11:07:45 +1000

    fix test

--- test/mpi/UtilityTests/ElementDiameterTests.jl
+++ test/mpi/UtilityTests/ElementDiameterTests.jl
@@ -7,12 +7,12 @@ using GridapGmsh,GridapDistributed, PartitionedArrays
 
 function main(distribute,mesh_partition)
   ranks = distribute(LinearIndices((prod(mesh_partition),)))
-  model = GmshDiscreteModel(ranks,"test/meshes/mesh_finer.msh")
+  model = GmshDiscreteModel(ranks,"test/meshes/mesh.msh")
 
   h = get_element_diameters(model)
   hₕ = get_element_diameter_field(model)
 
-  model_serial = GmshDiscreteModel("test/meshes/mesh_finer.msh")
+  model_serial = GmshDiscreteModel("test/meshes/mesh.msh")
   h_serial = get_element_diameters(model_serial)
   @test test_array(h_serial,collect(h))
   true
