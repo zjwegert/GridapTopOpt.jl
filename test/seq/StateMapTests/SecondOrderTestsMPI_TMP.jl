50e8a6c91eac1b0a108d9b2671d0a4ad52137863
zjwegert <60646897+zjwegert@users.noreply.github.com>
2026-04-29 20:54:58 +1000

    tmp

---
+++ test/seq/StateMapTests/SecondOrderTestsMPI_TMP.jl
@@ -0,0 +1,277 @@
+using Test, Gridap, GridapTopOpt
+using FiniteDifferences
+using Zygote
+using ForwardDiff
+using PartitionedArrays, GridapDistributed
+
+mesh_partition = (2,2)
+ranks = with_debug() do distribute
+  distribute(LinearIndices((prod(mesh_partition),)))
+end
+
+# FE setup
+order = 1
+xmax = ymax = 1.0
+dom = (0,xmax,0,ymax)
+el_size = (4,4)
+model = CartesianDiscreteModel(ranks,mesh_partition,dom,el_size)
+Ω = Triangulation(model)
+dΩ = Measure(Ω,2*order)
+reffe_scalar = ReferenceFE(lagrangian,Float64,order)
+V = TestFESpace(model,reffe_scalar;dirichlet_tags=[1,2,3,4,5,6]) # 3 dofs
+U = TrialFESpace(V,0.0)
+V_p = TestFESpace(model,reffe_scalar;dirichlet_tags=[2,3,4,5,6,7,8]) # 2 dofs
+
+#########################################
+# Second order partial derivative tests #
+#########################################
+
+J(u,p) = ∫(u*u*p*p)dΩ # keep p term otherwise dual error
+uh = interpolate(x->rand(), U)
+ph = interpolate(x->rand(), V_p)
+λh = interpolate(x->rand(), V)
+spaces = (U,V_p)
+_,_,_, ∂2J∂u2_mat, _, ∂2J∂u∂p_mat, _, ∂2J∂p2_mat, _, ∂2J∂p∂u_mat = GridapTopOpt.build_inc_obj_cache(J,uh,ph,spaces);
+#∂2J∂u2_mat, ∂2J∂u∂p_mat, ∂2J∂p2_mat, ∂2J∂p∂u_mat = SecondOrderTopOpt.incremental_objective_partials(J,uh,ph,spaces)
+
+# ∂²J / ∂u² * u̇
+dv = get_fe_basis(V)
+du = get_trial_fe_basis(U)
+dp = get_fe_basis(V_p)
+dp_ = get_trial_fe_basis(V_p)
+
+∂2∂u2_analytical(uh) = ∫( 2*ph*ph*du⋅dv )dΩ
+∂2∂u2_matrix_analytical = assemble_matrix(∂2∂u2_analytical(uh),U,U)
+@test reduce(&,map(≈,partition(∂2∂u2_matrix_analytical),partition(∂2J∂u2_mat)))
+
+# ∂/∂p (∂J/∂u ) * ṗ
+∂2J∂u∂p_analytical(uh,ph) = ∫( 4*ph*uh*dp_⋅dv )dΩ
+∂2J∂u∂p_matrix_analytical = assemble_matrix(∂2J∂u∂p_analytical(uh,ph),V_p,U)
+@test reduce(&,map(≈,partition(∂2J∂u∂p_matrix_analytical),partition(∂2J∂u∂p_mat)))
+
+# ∂²J / ∂p² * ṗ
+∂2J∂p2_analytical(uh) = ∫( 2*uh*uh*dp⋅dp_ )dΩ
+∂2J∂p2_matrix_analytical = assemble_matrix(∂2J∂p2_analytical(uh),V_p,V_p)
+@test reduce(&,map(≈,partition(∂2J∂p2_matrix_analytical),partition(∂2J∂p2_mat)))
+
+# ∂/∂u (∂J / ∂p) * u̇
+∂2J∂p∂u_analytical(uh,ph) = ∫( 4*uh*ph*du⋅dp )dΩ
+∂2J∂p∂u_matrix_analytical = assemble_matrix(∂2J∂p∂u_analytical(uh,ph),U,V_p)
+@test reduce(&,map(≈,partition(∂2J∂p∂u_matrix_analytical),partition(∂2J∂p∂u_mat)))
+
+f(x) = 1.0
+res(u,v,p) = ∫( p*∇(u)⋅∇(v) - f*v )dΩ
+state_map = NonlinearFEStateMap(res,U,V,V_p,diff_order=2)
+∂2R∂u2_mat, ∂2R∂u∂p_mat, ∂2R∂p2_mat, ∂2R∂p∂u_mat = GridapTopOpt.update_incremental_adjoint_partials!(state_map,uh,ph,λh)
+
+# ∂²R / ∂u² * u̇ * λ
+∂2∂u2R_analytical(uh,λh,ph) = ∫( 0*du*dv )dΩ
+∂2∂u2R_matrix_analytical = assemble_matrix(∂2∂u2R_analytical(uh,λh,ph),U,U)
+@test reduce(&,map(≈,partition(∂2∂u2R_matrix_analytical),partition(∂2R∂u2_mat)))
+
+# ∂/∂p (∂R/∂u * λ) * ṗ
+∂2R∂u∂p_analytical(uh,λh,ph) = ∫( dp_* ∇(dv) ⋅ ∇(λh)  )dΩ
+∂2R∂u∂p_matrix_analytical = assemble_matrix(∂2R∂u∂p_analytical(uh,λh,ph),V_p,U)
+@test reduce(&,map(≈,partition(∂2R∂u∂p_matrix_analytical),partition(∂2R∂u∂p_mat)))
+# ∂²R / ∂p² * ṗ * λ
+∂2R∂p2_analytical(uh,λh) = ∫( 0*dp⋅dp_ )dΩ
+∂2R∂p2_matrix_analytical = assemble_matrix(∂2R∂p2_analytical(uh,λh),V_p,V_p)
+@test reduce(&,map(≈,partition(∂2R∂p2_matrix_analytical),partition(∂2R∂p2_matrix_analytical)))
+
+# ∂/∂u (∂R/∂p * λ) * ṗ
+∂2R∂p∂u_analytical(uh,λh,ph) = ∫( dp * ∇(du) ⋅ ∇(λh) )dΩ
+∂2R∂p∂u_matrix_analytical = assemble_matrix(∂2R∂p∂u_analytical(uh,λh,ph),U,V_p)
+@test reduce(&,map(≈,partition(∂2R∂p∂u_matrix_analytical),partition(∂2R∂p∂u_mat)))
+
+######################
+# Self-adjoint tests #
+######################
+
+f(x) = 1.0
+res(u,v,p) = ∫( p*∇(u)⋅∇(v)-f*v )dΩ
+J(u,p) = ∫( f*u + 0*p )dΩ # p term to avoid dual error - should be fixed in the future
+state_map = NonlinearFEStateMap(res,U,V,V_p,diff_order=2)
+objective = GridapTopOpt.StateParamMap(J,state_map,diff_order=2)
+ph = interpolate(x->rand(), V_p)
+ṗh = interpolate(x->rand(), V_p)
+u = state_map(get_free_dof_values(ph))
+
+uh = FEFunction(U,u)
+Zygote.gradient(p->objective(state_map(p),p),get_free_dof_values(ph)) # update λ
+λ = state_map.cache.adj_cache[3]
+
+@test u ≈ λ # the adjoint should equal the solution for a self-adjoint problem
+
+λh = FEFunction(V,λ)
+T = ForwardDiff.Tag(()->(),typeof(p))
+p = get_free_dof_values(ph)
+ṗ = get_free_dof_values(ṗh)
+pᵋ = GridapTopOpt._build_duals(T,p,ṗ);
+uᵋ = state_map(pᵋ)
+ForwardDiff.value.(uᵋ) ≈ u
+
+function _mapreduce_partials(pᵋ::PVector)
+  v = map(Base.Fix2(GridapTopOpt._mapreduce_partials,nothing), local_views(pᵋ))
+  pvec_ids = pᵋ.index_partition
+  return PVector(v,pvec_ids)
+end
+
+u̇ = _mapreduce_partials(uᵋ)
+
+∇f = p->GridapTopOpt.val_and_gradient(p->objective(state_map(p),p),p).grad[1]
+Hṗ_FOR = ForwardDiff.derivative(α -> ∇f(p + α*ṗ), 0)
+λ⁻ = state_map.cache.inc_adjoint_cache[1]
+
+@test λ⁻ ≈ u̇ # the incremental adjoint should equal the incremental state for a self-adjoint problem
+
+# function p_to_j(p)
+#     ph = FEFunction(V_p,p)
+#     op = FEOperator((u,v)->res(u,v,ph),U,V)
+#     uh = solve(op)
+#     sum(J(uh,ph))
+# end
+# g_fd = p->FiniteDifferences.jacobian(central_fdm(5,1),p_to_j,p)
+# Hṗ_fd = FiniteDifferences.jacobian(central_fdm(5,1),g_fd,p)[1]*ṗ
+
+# p_to_j(p) = objective((state_map(p)),p)
+# @test Hṗ_fd ≈ Hvp(p_to_j,p,ṗ) # the Hessian-vector product computed using the pullback should match the finite difference approximation of the Hessian-vector product (this is a test of the entire incremental map, including the adjoint part)
+
+# ########################################################
+# # Unit and integration tests for the pushforward rules #
+# ########################################################
+
+# J(u,p) = ∫( f*(1.0(sin∘(2π*u))+1)*(1.0(cos∘(2π*p))+1)*p)dΩ
+# objective = GridapTopOpt.StateParamMap(J,state_map,diff_order=2)
+
+# # incremental objective (and pullback) test (u̇->du̇)
+# N = num_free_dofs(V)
+# function up_to_j(up)
+#     u = up[1:N]
+#     p = up[N+1:end]
+#     j = objective(u,p)
+# end
+# up = vcat(u,p)
+# u̇ṗ = vcat(u̇,ṗ)
+# ∇f = up->Zygote.gradient(up_to_j,up)[1]
+# du̇dṗ =  ForwardDiff.derivative(α -> ∇f(up + α*u̇ṗ), 0)
+# du̇dṗ_FD =FiniteDifferences.jacobian(central_fdm(5,1),up->Zygote.gradient(up_to_j,up)[1],up)[1]*vcat(u̇,ṗ)
+
+# @test du̇dṗ_FD ≈ du̇dṗ # the pullback of the incremental objective should match the finite difference approximation of the pullback of the incremental objective
+
+# # Nonlinear state map tests
+# res(u,v,p) = ∫( (u+1)*(p)*∇(u)⋅∇(v) - f*v )dΩ
+# state_map = NonlinearFEStateMap(res,U,V,V_p,diff_order=2)
+# Zygote.gradient(p->objective(state_map(p),p),p) # update λ and u
+
+# # incremental state test (ṗ->u̇)
+# function p_to_u(p)
+#     ph = FEFunction(V_p,p)
+#     op = FEOperator((u,v)->res(u,v,ph),U,V)
+#     uh = solve(op)
+#     return get_free_dof_values(uh)
+# end
+# uᵋ = state_map(pᵋ)
+# u̇ = vec(mapreduce(ForwardDiff.partials, hcat, uᵋ))
+# ∂u_∂p_FD = FiniteDifferences.jacobian(central_fdm(5,1),p_to_u,p)[1]
+# ∂u_∂p_FD_ṗ = ∂u_∂p_FD * ṗ
+# @test u̇ ≈ ∂u_∂p_FD_ṗ rtol = 1e-7 # the pullback of the incremental state should match the finite difference approximation of the pullback of the incremental state
+
+# # entire incremental map (including the adjoint part) (ṗ->dṗ)
+# function p_to_j(p)
+#     ph = FEFunction(V_p,p)
+#     op = FEOperator((u,v)->res(u,v,ph),U,V)
+#     uh = solve(op)
+#     sum(J(uh,ph))
+# end
+# g_fd = p->FiniteDifferences.jacobian(central_fdm(5,1),p_to_j,p)
+# Hṗ_fd = FiniteDifferences.jacobian(central_fdm(5,1),g_fd,p)[1]*ṗ
+
+# p_to_j(p) = objective((state_map(p)),p)
+# @test Hṗ_fd ≈ Hvp(p_to_j,p,ṗ) # the Hessian-vector product computed with AD should match the finite difference approximation of the Hessian-vector product (this is a test of the entire incremental map, including the adjoint part)
+
+# #Affine state map Tests
+# a(u,v,p) = ∫( p*(p+1)*∇(u)⋅∇(v) )dΩ
+# l(v,p) = ∫( f*v )dΩ
+# state_map = AffineFEStateMap(a,l,U,V,V_p,diff_order=2)
+# Zygote.gradient(p->objective(state_map(p),p),p) # update λ and u
+
+# # incremental state test (ṗ->u̇)
+# function p_to_u(p)
+#     ph = FEFunction(V_p,p)
+#     op = FEOperator((u,v)->a(u,v,ph)-l(v,ph),U,V)
+#     uh = solve(op)
+#     return get_free_dof_values(uh)
+# end
+# uᵋ = state_map(pᵋ)
+# u̇ = vec(mapreduce(ForwardDiff.partials, hcat, uᵋ))
+# ∂u_∂p_FD = FiniteDifferences.jacobian(central_fdm(5,1),p_to_u,p)[1]
+# ∂u_∂p_FD_ṗ = ∂u_∂p_FD * ṗ
+# @test u̇ ≈ ∂u_∂p_FD_ṗ
+
+# # entire incremental map (including the adjoint part) (ṗ->dṗ)
+# function p_to_j(p)
+#     ph = FEFunction(V_p,p)
+#     op = AffineFEOperator((u,v)->a(u,v,ph),v->l(v,ph),U,V)
+#     uh = solve(op)
+#     sum(J(uh,ph))
+# end
+# g_fd = p->FiniteDifferences.jacobian(central_fdm(5,1),p_to_j,p)
+# Hṗ_fd = FiniteDifferences.jacobian(central_fdm(5,1),g_fd,p)[1]*ṗ
+
+# p_to_j(p) = objective((state_map(p)),p)
+# @test Hṗ_fd ≈ Hvp(p_to_j,p,ṗ) # the Hessian-vector product computed using AD should match the finite difference approximation of the Hessian-vector product (this is a test of the entire incremental map, including the adjoint part)
+
+##
+mesh_partition = (2,2)
+ranks = with_debug() do distribute
+  distribute(LinearIndices((prod(mesh_partition),)))
+end
+
+function driver(model)
+  f(x) = x[2]
+  g(x) = x[1]
+
+  Ω = Triangulation(model)
+  dΩ = Measure(Ω, 2)
+  reffe = ReferenceFE(lagrangian, Float64, 1)
+  K = TestFESpace(model, reffe)
+  V = TestFESpace(model, reffe; dirichlet_tags="boundary")
+  U = TrialFESpace(V,g)
+  a(u, v, κ) = ∫(κ * ∇(v) ⋅ ∇(u))dΩ
+  b(v, κ) = ∫(v*f)dΩ
+  κ_to_u = AffineFEStateMap(a,b,U,V,K;diff_order=2)
+  l2_norm = StateParamMap((u, κ) -> ∫(u ⋅ u + 0κ)dΩ,κ_to_u;diff_order=2) # (!!)
+  u_obs = interpolate(x -> sin(2π*x[1]), V) |> get_free_dof_values
+  function J(κ)
+    u = κ_to_u(κ)
+    sqrt(l2_norm(u-u_obs, κ))
+  end
+  κ0h = interpolate(1.0, K)
+  val, grad = val_and_gradient(J, get_free_dof_values(κ0h));
+  # Hessian-vector product
+  vh = interpolate(0.5, K);
+  Hv = Hvp(J, get_free_dof_values(κ0h),get_free_dof_values(vh));
+
+  return Hv, K
+end
+
+model_serial = CartesianDiscreteModel((0,1,0,1),(8,8));
+dF_serial,V_deriv_serial = driver(model_serial);
+
+model = GridapTopOpt.ordered_distributed_model_from_serial_model(ranks,model_serial);
+dF,V_deriv = driver(model);
+
+# @test
+length(dF_serial) ≈ length(dF)
+# @test
+norm(dF_serial)
+norm(dF)
+
+dFh = FEFunction(V_deriv,dF)
+dFh_serial = FEFunction(V_deriv_serial,dF_serial)
+deriv_test = GridapTopOpt.test_serial_and_distributed_fields(dFh,V_deriv,dFh_serial,V_deriv_serial)
+
+map_main(deriv_test) do deriv_test
+  @test deriv_test
+  nothing
+end
