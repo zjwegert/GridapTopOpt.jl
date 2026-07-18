0308f04e5dbe0447005800a3ded1b30cb3c32b5e
zjwegert <60646897+zjwegert@users.noreply.github.com>
2026-07-19 08:24:08 +1000

    assemble conj matrix

--- src/StateMaps/FEStateMaps.jl
+++ src/StateMaps/FEStateMaps.jl
@@ -220,7 +220,7 @@ function pullback(φ_to_u::AbstractFEStateMap,uh,φh,du;updated=false)
     update_adjoint_caches!(φ_to_u,uh,φh)
   end
 
-  λ  = adjoint_solve!(φ_to_u,du)
+  λ  = conj!(adjoint_solve!(φ_to_u,conj!(du)))
   λh = FEFunction(get_test_space(φ_to_u),λ)
 
   update_incremental_adjoint_partials!(φ_to_u,uh,φh,λh)
